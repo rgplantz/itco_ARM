fa3eaa85ad50f63e35796dda05764d4fae8c6a5a
Bob Plantz <rgplantz@outlook.com>
2021-12-21 16:14:42 -0800

    Add echoChar.s

---
+++ chapter_10/Text_Listings/doNothingProg_asm/doNothingProg.s
@@ -0,0 +1,12 @@
+// doNothingProg.s
+// Minimum components of a C program, in assembly language
+
+        .arch   armv8-a
+
+        .text
+        .align  2
+        .global main
+        .type   main, %function
+main:
+        mov     w0, 0           // return 0;
+        ret
