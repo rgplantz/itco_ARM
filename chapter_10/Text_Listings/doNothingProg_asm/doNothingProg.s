8f3a27c425b29c87a6c351322b396371891692c9
rgplantz <rgplantz@outlook.com>
2022-08-31 19:17:57 -0700

    Changed doNothing.s to use zero register.

--- chapter_10/Text_Listings/doNothingProg_asm/doNothingProg.s
+++ chapter_10/Text_Listings/doNothingProg_asm/doNothingProg.s
@@ -2,11 +2,10 @@
 // Minimum components of a C program, in assembly language
 
         .arch   armv8-a
-
         .text
         .align  2
         .global main
         .type   main, %function
 main:
-        mov     w0, 0           // return 0;
+        mov     w0, wzr         // return 0;
         ret
