0c8ead713f8053e5daedeaad9e107487bc4d8027
rgplantz <rgplantz@outlook.com>
2022-09-28 16:15:16 -0700

    Simplify addressing

--- chapter_11/Text_Listings/incInt_asm/incInt.s
+++ chapter_11/Text_Listings/incInt_asm/incInt.s
@@ -20,18 +20,15 @@ result:
 main:
         stp     fp, lr, [sp, frameSize]!  // our stack frame
         mov     fp, sp
-        adrp    x0, prompt                // prompt user
-        add     x0, x0, :lo12:prompt
+        adr     x0, prompt                // prompt user
         bl      printf
         add     x1, sp, x                 // address for input
-        adrp    x0, inputFormat           // scanf format string
-        add     x0, x0, :lo12:inputFormat
+        adr     x0, inputFormat           // scanf format string
         bl      scanf
         ldr     w0, [sp, x]               // get x
         add     w1, w0, 1                 // add one
         str     w1, [sp, x]               // x++;
-        adrp    x0, result                // address of format string
-        add     x0, x0, :lo12:result
+        adr     x0, result                // address of format string
         bl      printf
         mov     w0, wzr
         ldp     fp, lr, [sp], frameSize // undo stack frame
