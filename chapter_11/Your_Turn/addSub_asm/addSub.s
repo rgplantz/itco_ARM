d52dc1888c88fff0639a3d4258a668febd778b1c
rgplantz <84108577+rgplantz@users.noreply.github.com>
2022-09-29 20:40:12 -0700

    Update addSub.s

--- chapter_11/Your_Turn/addSub_asm/addSub.s
+++ chapter_11/Your_Turn/addSub_asm/addSub.s
@@ -14,7 +14,7 @@ prompt:
 inputFormat:
         .string "%i"
 result:
-        .string "Result: %i\n"
+        .string "Sum = %i, Difference = %i\n"
 // Code
         .text
         .align  2
@@ -23,16 +23,34 @@ result:
 main:
         stp     fp, lr, [sp, frameSize]!  // our stack frame
         mov     fp, sp
+
         adr     x0, prompt                // prompt user
         bl      printf
         add     x1, sp, x                 // address for input
         adr     x0, inputFormat           // scanf format string
         bl      scanf
+
+        adr     x0, prompt                // prompt user
+        bl      printf
+        add     x1, sp, y                 // address for input
+        adr     x0, inputFormat           // scanf format string
+        bl      scanf
+
         ldr     w0, [sp, x]               // get x
-        add     w1, w0, 1                 // add one
-        str     w1, [sp, x]               // x++;
+        ldr     w1, [sp, y]               // and y
+        add     w3, w0, w1                // add them
+        str     w3, [sp, sum]             // sum = x + y
+
+        ldr     w0, [sp, x]               // get x
+        ldr     w1, [sp, y]               // and y
+        sub     w3, w0, w1                // subtract them
+        str     w3, [sp, diff]            // diff = x - y
+
+        ldr     w2, [sp, sum]             // sum
+        ldr     w1, [sp, sum]             // difference
         adr     x0, result                // address of format string
         bl      printf
+
         mov     w0, wzr
-        ldp     fp, lr, [sp], frameSize // undo stack frame
+        ldp     fp, lr, [sp], frameSize   // undo stack frame
         ret
