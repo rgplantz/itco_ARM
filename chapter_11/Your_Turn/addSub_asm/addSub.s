11db6ba300af7a1d262e9dfa271888dc6f9f21b1
rgplantz <84108577+rgplantz@users.noreply.github.com>
2022-09-29 20:13:03 -0700

    Add addSub program for Chap 11

--- chapter_11/Your_Turn/addSub_asm/addSub.s
+++ chapter_11/Your_Turn/addSub_asm/addSub.s
@@ -1,8 +1,11 @@
-// incInt.s
-// Increments an integer.
+// addSub.s
+// Adds and subtracts two integers.
         .arch armv8-a
 // Stack frame
-        .equ    x, 28
+        .equ    x, 16
+        .equ    y, 20
+        .equ    sum, 24
+        .equ    diff, 28
         .equ    frameSize, 32
 // Constants 
         .section        .rodata
@@ -20,21 +23,16 @@ result:
 main:
         stp     fp, lr, [sp, frameSize]!  // our stack frame
         mov     fp, sp
-
         adr     x0, prompt                // prompt user
         bl      printf
-
         add     x1, sp, x                 // address for input
         adr     x0, inputFormat           // scanf format string
         bl      scanf
-
         ldr     w0, [sp, x]               // get x
         add     w1, w0, 1                 // add one
         str     w1, [sp, x]               // x++;
-
         adr     x0, result                // address of format string
         bl      printf
-
         mov     w0, wzr
         ldp     fp, lr, [sp], frameSize // undo stack frame
         ret
