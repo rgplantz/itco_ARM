9ae8d0cb750ba0514391c44f12c0fd8e1bae38d4
rgplantz <rgplantz@outlook.com>
2022-10-08 20:35:23 -0700

    Update printInt.s

--- chapter_12/Text_Listings/printInt/printInt.s
+++ chapter_12/Text_Listings/printInt/printInt.s
@@ -3,7 +3,7 @@
         .arch armv8-a
 // Stack frame
         .equ    x, 28
-        .equ    frameSize, 32
+        .equ    frame, 32
 // Constants 
         .section        .rodata
 result:
@@ -14,20 +14,20 @@ result:
         .global main
         .type   main, %function
 main:
-        stp     fp, lr, [sp, frameSize]!  // our stack frame
+        stp     fp, lr, [sp, frame]!  // our stack frame
         mov     fp, sp
 
-        mov     w0, 0x1234                // a constant
+        mov     w0, 0x1234            // a constant
         str     w0, [sp, x]
 
-        ldr     w21, [sp, x]              // get x
-        mov     w1, w21                   // second argument
-        add     w1, w21, 0                // same instruction?
-        adrp    x0, result                // page of format string
-        add     x0, x0, :lo12:result      // plus offset in page
-        adr     x0, result                // show other way
+        ldr     w21, [sp, x]          // get x
+        mov     w1, w21               // second argument
+        add     w1, w21, 0            // same instruction?
+        adrp    x0, result            // page of format string
+        add     x0, x0, :lo12:result  // plus offset in page
+        adr     x0, result            // show other way
         bl      printf
 
         mov     w0, wzr
-        ldp     fp, lr, [sp], frameSize // undo stack frame
+        ldp     fp, lr, [sp], frame   // undo stack frame
         ret
