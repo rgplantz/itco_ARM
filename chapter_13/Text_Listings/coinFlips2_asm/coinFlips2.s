80832c0447ce4ac8c7c4e797ce1f9d2ed70449f0
rgplantz <rgplantz@outlook.com>
2022-10-21 11:38:49 -0700

    Update two asm programs

--- chapter_13/Text_Listings/coinFlips2_asm/coinFlips2.s
+++ chapter_13/Text_Listings/coinFlips2_asm/coinFlips2.s
@@ -5,7 +5,7 @@
         .equ    NTIMES,10                 // number of flips
         .equ    MIDDLE,1073741823         // RAND_MAX/2
 // Stack frame
-        .equ    frameSize,32
+        .equ    frame,32
 // Code
         .text
         .section        .rodata
@@ -20,31 +20,29 @@ tailsMsg:
         .global main
         .type   main, %function
 main:
-        stp     fp, lr, [sp, -frameSize]! // create our stack frame
-        mov     fp, sp                    // set our frame pointer
-        str     w19, [sp, 28]             // save for i local var.
-        mov     w19, 0                    // i = 0;
+        stp     fp, lr, [sp, -frame]! // create our stack frame
+        mov     fp, sp                // set our frame pointer
+        str     w19, [sp, 28]         // save for i local var.
+        mov     w19, 0                // i = 0;
 forLoop:
-        mov     w0, NTIMES                // total number of times
-        cmp     w19, w0                   // is i at end?
-        b.hs    allDone                   // yes
-        bl      random                    // no, get random number
-        mov     w1, MIDDLE                // half way
-        cmp     w1, w0                    // above or below middle?
-        bgt     tails                     // above -> tails
-        adrp    x0, headsMsg              // below -> heads message
-        add     x0, x0, :lo12:headsMsg    // message offset in page
-        bl      puts                      // print message
-        b       continue                  // skip else part
+        mov     w0, NTIMES            // total number of times
+        cmp     w19, w0               // is i at end?
+        b.hs    allDone               // yes
+        bl      random                // no, get random number
+        mov     w1, MIDDLE            // half way
+        cmp     w1, w0                // above or below middle?
+        b.gt    tails                 // above -> tails
+        adr     x0, headsMsg          // below -> heads message
+        bl      puts                  // print message
+        b       continue              // skip else part
 tails:
-        adrp    x0, tailsMsg              // tails message page address
-        add     x0, x0, :lo12:tailsMsg    // message offset in page
-        bl      puts                      // print message
+        adr     x0, tailsMsg          // tails message page address
+        bl      puts                  // print message
 continue:
-        add     w19, w19, 1               // i++;
-        b       forLoop                   // and continue loop
+        add     w19, w19, 1           // i++;
+        b       forLoop               // and continue loop
 allDone:
-        mov     w0, 0                     // return 0
-        ldr     w19, [sp, 28]             // restore regs.
-        ldp     fp, lr, [sp], frameSize   // restore fp, lr, sp
-        ret                               // back to caller
+        mov     w0, wzr               // return 0
+        ldr     w19, [sp, 28]         // restore regs.
+        ldp     fp, lr, [sp], frame   // restore fp, lr, sp
+        ret                           // back to caller
--- chapter_13/Text_Listings/helloWorld-while_asm/helloWorld.s
+++ chapter_13/Text_Listings/helloWorld-while_asm/helloWorld.s
@@ -5,7 +5,7 @@
         .equ    NUL,wzr                   // use zero register
         .equ    STDOUT,1
 // Stack frame
-        .equ    frameSize,16
+        .equ    frame,16
 // Code
         .text
         .section        .rodata
@@ -17,22 +17,21 @@ message:
         .global main
         .type   main, %function
 main:
-        stp     fp, lr, [sp, -frameSize]! // create our stack frame
-        mov     fp, sp                    // set our frame pointer
-        mov     x2, promptLength          // number of characters
-        adrp    x0, message               // address of page
-        add     x19, x0, :lo12:message    // plus offset in page
+        stp     fp, lr, [sp, -frame]! // create our stack frame
+        mov     fp, sp                // set our frame pointer
+        mov     x2, promptLength      // number of characters
+        adr     x0, message           // address of page
 whileLoop:
-        ldrb    w0, [x19]                 // load character
-        cmp     w0, NUL                   // end of string?
-        beq     allDone                   // yes
-        mov     w2, STDOUT                // no, one char
-        mov     x1, x19                   // address of char
-        mov     x0, STDOUT                // write on screen
+        ldrb    w0, [x19]             // load character
+        cmp     w0, NUL               // end of string?
+        b.eq    allDone               // yes
+        mov     w2, STDOUT            // no, one char
+        mov     x1, x19               // address of char
+        mov     x0, STDOUT            // write on screen
         bl      write
-        add     x19, x19, 1               // increment pointer
-        b       whileLoop                 // and continue
+        add     x19, x19, 1           // increment pointer
+        b       whileLoop             // and continue
 allDone:
-        mov     w0, 0                     // return value
-        ldp     fp, lr, [sp], frameSize   // undo our stack frame
+        mov     w0, wzr               // return value
+        ldp     fp, lr, [sp], frame   // undo our stack frame
         ret
