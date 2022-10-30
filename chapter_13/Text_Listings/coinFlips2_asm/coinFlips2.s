93e2d16c6ac450a2ee2bf88815104336b2679126
rgplantz <rgplantz@outlook.com>
2022-10-30 15:08:13 -0700

    Update coinFlips2.s

--- chapter_13/Text_Listings/coinFlips2_asm/coinFlips2.s
+++ chapter_13/Text_Listings/coinFlips2_asm/coinFlips2.s
@@ -2,17 +2,16 @@
 // Flips a coin, heads or tails.
         .arch armv8-a
 // Useful constants
-        .equ    NTIMES,10                 // number of flips
-        .equ    MIDDLE,1073741823         // RAND_MAX/2
+        .equ    NTIMES,10             // number of flips
+        .equ    MIDDLE,1073741823     // RAND_MAX/2
 // Stack frame
+        .equ    save19, 28
         .equ    frame,32
 // Code
         .text
         .section        .rodata
-        .align  3
 headsMsg:
         .string "heads"
-        .align  3
 tailsMsg:
         .string "tails"
         .text
@@ -22,8 +21,8 @@ tailsMsg:
 main:
         stp     fp, lr, [sp, -frame]! // create our stack frame
         mov     fp, sp                // set our frame pointer
-        str     w19, [sp, 28]         // save for i local var.
-        mov     w19, 0                // i = 0;
+        str     w19, [sp, save19]     // save for i local var.
+        mov     w19, wzr              // i = 0;
 forLoop:
         mov     w0, NTIMES            // total number of times
         cmp     w19, w0               // is i at end?
@@ -43,6 +42,6 @@ continue:
         b       forLoop               // and continue loop
 allDone:
         mov     w0, wzr               // return 0
-        ldr     w19, [sp, 28]         // restore regs.
+        ldr     w19, [sp, save19]     // restore reg.
         ldp     fp, lr, [sp], frame   // restore fp, lr, sp
         ret                           // back to caller
