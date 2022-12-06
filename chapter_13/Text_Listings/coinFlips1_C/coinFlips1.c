4da6a2f49586dd395b84a4ed29853a88f903fee9
rgplantz <rgplantz@outlook.com>
2022-12-06 08:21:25 -0800

    Fix tabs

--- chapter_13/Text_Listings/coinFlips1_C/coinFlips1.c
+++ chapter_13/Text_Listings/coinFlips1_C/coinFlips1.c
@@ -5,17 +5,15 @@
 #include <stdlib.h>
 
 int main() {
-register int randomNumber;
-  register int i;
+    register int randomNumber;
+    register int i;
 
-  for (i = 0; i < 10; i++)
-  {
-    randomNumber = random();
-    if (randomNumber < RAND_MAX/2)
-    {
-      puts("heads");
+    for (i = 0; i < 10; i++) {
+        randomNumber = random();
+        if (randomNumber < RAND_MAX/2) {
+            puts("heads");
+        }
     }
-  }
 
-  return 0;
+    return 0;
 }
