e46da8eb09b5f91e6d8622509590222915e066e4
rgplantz <rgplantz@outlook.com>
2022-12-10 08:00:55 -0800

    Add message to addEleven

--- chapter_14/Text_Listings/sum11ints_C/addEleven.c
+++ chapter_14/Text_Listings/sum11ints_C/addEleven.c
@@ -1,12 +1,14 @@
 // addEleven.c
 // Sums Eleven integers and returns the sum.
 
- #include "addEleven.h"
+#include <stdio.h>
+#include "addEleven.h"
 
 int addEleven(int one, int two, int three, int four, int five, int six,
                 int seven, int eight, int nine, int ten, int eleven) {
     int sum;
 
+    puts("Adding them.");
     sum = one + two + three + four + five + six
             + seven + eight + nine + ten + eleven;
     return sum;
