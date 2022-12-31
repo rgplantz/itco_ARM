0647c9adbd30c5b70adef717a84e49b3aeaf4ee3
rgplantz <rgplantz@outlook.com>
2022-12-31 14:35:16 -0800

    Add sum 11 ints asm

---
+++ chapter_14/Text_Listings/sum11ints_asm/addEleven.s
@@ -0,0 +1,73 @@
+// addEleven.s
+// Adds eleven integers and returns sum.
+// Calling sequence:
+//    w0 through w7 <- 8 integers
+//    [sp] <- integer
+//    [sp+8] <- integer
+//    [sp+16] <- integer
+//    returns sum
+        .arch armv8-a
+// Stack frame
+        .equ    eight, 16
+        .equ    seven, 20
+        .equ    six, 24
+        .equ    five, 28
+        .equ    four, 32
+        .equ    three, 36
+        .equ    two, 40
+        .equ    one, 44
+        .equ    sum, 60
+        .equ    nine, 64
+        .equ    ten, 72
+        .equ    eleven, 80
+        .equ    frame, 96
+// Constant data
+        .section        .rodata
+        .align  3
+msg:
+        .string "Added them"
+        .text
+        .align  2
+        .global addEleven
+        .type   addEleven, %function
+addEleven:
+        stp     fp, lr, [sp, -frame]! // create our stack frame
+        mov     fp, sp                // set our frame pointer
+
+        str     w0, [sp, one]         // save register args.
+        str     w1, [sp, two]
+        str     w2, [sp, three]
+        str     w3, [sp, four]
+        str     w4, [sp, five]
+        str     w5, [sp, six]
+        str     w6, [sp, seven]
+        str     w7, [sp, eight]
+
+        ldr     w1, [sp, one]         // load args.             
+        ldr     w0, [sp, two]
+        add     w1, w1, w0            //      and sum them
+        ldr     w0, [sp, three]
+        add     w1, w1, w0
+        ldr     w0, [sp, four]
+        add     w1, w1, w0
+        ldr     w0, [sp, five]
+        add     w1, w1, w0
+        ldr     w0, [sp, six]
+        add     w1, w1, w0
+        ldr     w0, [sp, seven]
+        add     w1, w1, w0
+        ldr     w0, [sp, eight]
+        add     w1, w1, w0
+        ldr     w0, [sp, nine]
+        add     w1, w1, w0
+        ldr     w0, [sp, ten]
+        add     w0, w1, w0
+        ldr     w1, [sp, addEleven]
+        add     w0, w1, w0
+        str     w0, [sp, sum]         // store sum
+        adr     x0, msg               // tell user we're done
+        bl      puts
+
+        ldr     w0, [sp, sum]         // return the sum
+        ldp     fp, lr, [sp], frame   // restore fp, lr, sp
+        ret                           // back to caller
---
+++ chapter_14/Text_Listings/sum11ints_asm/sum11ints.s
@@ -0,0 +1,77 @@
+// .ssum11ints.s
+// Sums the integers 1 - 11.
+        .arch armv8-a
+// Stack frame
+        .equ    arg11, 0
+        .equ    arg10, 8
+        .equ    arg11, 12
+        .equ    total, 48
+        .equ    k, 52
+        .equ    j, 56
+        .equ    i, 60
+        .equ    h, 64
+        .equ    g, 68
+        .equ    f, 72
+        .equ    e, 76
+        .equ    d, 80
+        .equ    c, 84
+        .equ    b, 88
+        .equ    a, 92
+        .equ    frame, 96
+// Constant data
+        .section        .rodata
+        .align  3
+format:
+        .string "The sum is %i\n"
+        .text
+        .align  2
+        .global main
+        .type   main, %function
+main:
+        stp     fp, lr, [sp, -frame]! // create our stack frame
+        add     fp, sp, local         // set our frame pointer
+        mov     w0, 1                 // store values in local vars.
+        str     w0, [sp, a]
+        mov     w0, 2
+        str     w0, [sp, b]
+        mov     w0, 3
+        str     w0, [sp, c]
+        mov     w0, 4
+        str     w0, [sp, d]
+        mov     w0, 5
+        str     w0, [sp, e]
+        mov     w0, 6
+        str     w0, [sp, f]
+        mov     w0, 7
+        str     w0, [sp, g]
+        mov     w0, 8
+        str     w0, [sp, h]
+        mov     w0, 9
+        str     w0, [sp, i]
+        mov     w0, 10
+        str     w0, [sp, j]
+        mov     w0, 11
+        str     w0, [sp, k]
+        ldr     w0, [sp, k]           // store args 9 - 11
+        str     w0, [sp, arg11]       //      on stack
+        ldr     w0, [sp, j]
+        str     w0, [sp, arg10]
+        ldr     w0, [sp, i]
+        str     w0, [sp, arg9]
+        ldr     w7, [sp, h]           // load args 1 - 8
+        ldr     w6, [sp, g]           //      in regs. 0 - 7
+        ldr     w5, [sp, f]
+        ldr     w4, [sp, e]
+        ldr     w3, [sp, d]
+        ldr     w2, [sp, c]
+        ldr     w1, [sp, b]
+        ldr     w0, [sp, a]
+        bl      addEleven             // add all
+        str     w0, [sp, total]       // store returned sum
+        ldr     w1, [sp, total]       // argument to printf
+        adr     x0, format            // format string 
+        bl      printf                // print result
+        mov     w0, wzr               // return 0;
+
+        ldp     fp, lr, [sp], frame   // restore fp, lr, sp
+        ret                           // back to caller
