8a1d2580203d77d5d1e23edf2f93d2358b115053
rgplantz <rgplantz@outlook.com>
2023-01-07 11:47:33 -0800

    Revert "Changed sumInts program"
    This reverts commit 369ea5bcd4575d4b3853efc589ee97076574c1f6.

---
+++ chapter_15/Text_Listings/sumIntegers_C_of/addTwo.c
@@ -0,0 +1,18 @@
+// addTwo.c
+// Adds two integers; returns overflow T or F
+
+#include "addTwo.h"
+
+int addTwo(int a, int b, int *c) {
+    int sum;
+    int overflow = 0;   // assume no overflow
+  
+    sum = a + b;
+    if (((a > 0) && (b > 0) && (sum < 0)) ||
+            ((a < 0) && (b < 0) && (sum > 0))) {
+        overflow = 1;
+    }
+    *c = sum;
+
+    return overflow;
+}
---
+++ chapter_15/Text_Listings/sumIntegers_C_of/addTwo.h
@@ -0,0 +1,7 @@
+// addTwo.h
+// Adds two integers; returns overflow T or F
+
+#ifndef ADDTWO_H
+#define ADDTWO_H
+int addTwo(int a, int b, int *c);
+#endif
