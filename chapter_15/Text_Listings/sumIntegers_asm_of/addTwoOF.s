57c184d16870d14e06534ab18b1c534260da681e
rgplantz <rgplantz@outlook.com>
2023-01-11 10:48:46 -0800

    Create addTwoOF.s

---
+++ chapter_15/Text_Listings/sumIntegers_asm_of/addTwoOF.s
@@ -0,0 +1,18 @@
+// addTwoOF.s
+// Adds two integers outputs; returns overflow T or F
+// Calling sequence:
+//    w0 <- integer a
+//    w0 <- integer b
+//    x2 <- address for ouput
+//    returns 1 for overflow, 0 for no overflow
+        .arch armv8-a
+// Code
+        .text
+        .align  2
+        .global addTwo
+        .type   addTwo, %function
+addTwo:
+        adds    w1, w0, w1            // add, set condition flags
+        cinc    w0, wzr, vs           // overflow flag
+        str     w1, [x3]              // store output
+        ret                           // back to caller
