fdacbb33971979e4639b615d2e264ebf979079a1
rgplantz <rgplantz@outlook.com>
2023-01-15 12:13:07 -0800

    Fix chap 15 Your Turn

--- chapter_15/Your_Turn/sumUIntegers/addTwoCF.h
+++ chapter_15/Your_Turn/sumUIntegers/addTwoCF.h
@@ -1,8 +1,7 @@
-/* addTwoCF.h
- * Adds two unsigned integers and determines carry.
- */
+// addTwoCF.h
+// Adds two unsigned integers and determines carry
 
 #ifndef ADDTWOCF_H
 #define ADDTWOCF_H
-int addTwoCF(unsigned int a, unsigned int b, unsigned int *c);
+unsigned int addTwoCF(unsigned int a, unsigned int b, unsigned int *c);
 #endif
