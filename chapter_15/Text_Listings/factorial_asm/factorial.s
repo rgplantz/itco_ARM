1e6697faf1211b303a5c21b309219d9d4adf835e
rgplantz <rgplantz@outlook.com>
2023-01-26 08:35:28 -0800

    Update factorial.s

--- chapter_15/Text_Listings/factorial_asm/factorial.s
+++ chapter_15/Text_Listings/factorial_asm/factorial.s
@@ -5,7 +5,7 @@
 //    returns n!
         .arch armv8-a
 // Stack frame
-        .equ    n, 16
+        .equ    saveReg, 16
         .equ    frame, 32
 // Code
         .text
@@ -15,15 +15,16 @@
 factorial:
         stp     fp, lr, [sp, -frame]! // create our stack frame
         mov     fp, sp                // set our frame pointer
+        str     w0, [sp, n]           // w19, [sp, saveReg]    // save, use for n
 
-        str     w0, [sp, n]           // save n
-        mov     w1, w0                // and make a copy
+        mov     w19, w0               // copy n
         mov     w0, 1                 // assume base case, 0! = 1
-        cbz     w1, baseCase          // check for base case
-        sub     w0, w1, 1             // no,
-        bl      factorial             //    compute (n - 1)!
+        cbz     w19, baseCase         // check for base case
+        sub     w0, w19, 1            // no, n - 1
+        bl      factorial             // compute (n - 1)!
         ldr     w1, [sp, n]           // get n
-        mul     w0, w0, w1            // n x (n - 1)!
+        mul     w0, w0, w19           // n x (n - 1)!
 baseCase:
+        ldr     x19, [sp, saveReg]    // restore x19
         ldp     fp, lr, [sp], frame   // restore fp, lr, sp
         ret                           // back to caller
