9c174933ab8171377276be4471ca33df54c451d8
rgplantz <rgplantz@outlook.com>
2023-02-15 18:13:07 -0800

    Update upperCase.s

--- chapter_16/Text_Listings/upperCase_asm/upperCase.s
+++ chapter_16/Text_Listings/upperCase_asm/upperCase.s
@@ -4,7 +4,7 @@
         .equ    MAX,50                    // character storage limit
 // Stack frame
         .equ    theString, 16
-        .equ    frameSize, 80
+        .equ    frame, 80
 // Code
         .text
         .section        .rodata
@@ -20,26 +20,26 @@ newline:
         .global main
         .type   main, %function
 main:
-        stp     fp, lr, [sp, -frameSize]! // create stack frame
-        mov     fp, sp                    // our frame pointer
-        adr     x0, prompt                // prompt message
-        bl      writeStr                  // ask for input
+        stp     fp, lr, [sp, -frame]! // create stack frame
+        mov     fp, sp                // our frame pointer
+        adr     x0, prompt            // prompt message
+        bl      writeStr              // ask for input
 
-        add     x0, sp, theString         // place to store string
-        mov     w1, MAX                   // limit number of input chars
-        bl      readLn                    // get from keyboard
+        add     x0, sp, theString     // place to store string
+        mov     w1, MAX               // limit number of input chars
+        bl      readLn                // get from keyboard
 
-        add     x1, sp, theString         // address of string
-        mov     x0, x1                    // replace it
-        bl      toUpper                   // do conversion
+        add     x1, sp, theString     // address of string
+        mov     x0, x1                // replace it
+        bl      toUpper               // do conversion
 
-        adr     x0, result                // show result
+        adr     x0, result            // show result
         bl      writeStr
-        add     x0, sp, theString         // converted string
+        add     x0, sp, theString     // converted string
         bl      writeStr
-        adr     x0, newline               // nice formatting
+        adr     x0, newline           // nice formatting
         bl      writeStr
 
-        mov     w0, 0                     // return 0;
-        ldp     x29, x30, [sp], frameSize // undo stack frame
+        mov     w0, 0                 // return 0;
+        ldp     x29, x30, [sp], frame // undo stack frame
         ret
