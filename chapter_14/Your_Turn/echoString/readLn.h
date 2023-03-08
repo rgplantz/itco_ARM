e18d4e532ab094b134429b84a59a54ad9c01d290
rgplantz <rgplantz@outlook.com>
2023-03-08 11:04:58 -0800

    Update echoString program

--- chapter_14/Your_Turn/echoString/readLn.h
+++ chapter_14/Your_Turn/echoString/readLn.h
@@ -1,13 +1,11 @@
-/* readLn.h
- * C header file for readLn assembly language function
- * Reads a line (through the '\n' character from standard input. Deletes
- * the '\n' and creates a C-style text string.        
- * Calling sequence:
- *       rsi <- length of char array
- *       rdi <- address of place to store string
- *       call    readLn
- * returns number of characters read (not including NUL)
- */
+// Reads a line (through the '\n') from standard input. Has
+// a size limit. Extra characters and '\n' are ignored. Stores
+// NUL-terminated C string.
+// Calling sequence:
+//        x0 <- address of place to store string
+//        w1 <- string buffer size
+// returns number of characters read
+
 #ifndef READLN_H
 #define READLN_H
 int readLn(char *, int);
