da7db83976bfc56b4e422b5b80f35db00bc8543d
rgplantz <rgplantz@outlook.com>
2023-03-09 20:18:29 -0800

    Update convertHex.c

--- chapter_16/Text_Listings/convertHex_C/convertHex.c
+++ chapter_16/Text_Listings/convertHex_C/convertHex.c
@@ -2,17 +2,18 @@
 
 #include <stdio.h>
 #include "writeStr.h"
-#include "readLn.h"
+#include "readStr.h"
 #include "hexToInt.h"
 
-#define MAX 17
+#define MAX 16
+#define BUFFSZ MAX+1
 
 int main() {
-    char theString[MAX];
+    char theString[BUFFSZ];
     long int theInt;
     
     writeStr("Enter up to 16 hex characters: ");
-    readLn(theString, MAX);
+    readStr(theString, MAX);
 
     hexToInt(theString, &theInt);
     printf("%lx = %li\n", theInt, theInt);
