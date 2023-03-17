825f3c117754acd49e4d83a9da88211fb2e4ddb6
rgplantz <rgplantz@outlook.com>
2023-03-16 20:38:41 -0700

    update convertDec program

--- chapter_16/Text_Listings/convertDecimal_C/decToUInt.h
+++ chapter_16/Text_Listings/convertDecimal_C/decToUInt.h
@@ -1,10 +1,7 @@
-/* decToUInt.h
- * Converts decimal character string to unsigned int.
- * Returns number of characters.
- */
+// Converts decimal character string to unsigned int.
+// Returns number of characters.
  
 #ifndef DECTOUINT_H
 #define DECTOUINT_H
 int decToUInt(unsigned int *intPtr, char *stringPtr);
 #endif
-
