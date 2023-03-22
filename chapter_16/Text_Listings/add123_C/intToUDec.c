37bf1e77feadc117683d70657426c51859536737
rgplantz <rgplantz@outlook.com>
2023-03-22 13:59:52 -0700

    Update intToUDec.c

--- chapter_16/Text_Listings/add123_C/intToUDec.c
+++ chapter_16/Text_Listings/add123_C/intToUDec.c
@@ -1,4 +1,4 @@
-// Converts an int to corresponding unsigned decimal text
+// Converts an unsigned int to corresponding decimal text
 // string representation. Returns number of characters.
 
 #include "intToUDec.h"
