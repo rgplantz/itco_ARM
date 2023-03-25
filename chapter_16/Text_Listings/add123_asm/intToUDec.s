659f07d1a114e343d7bbe017329ea7bfa09348ca
rgplantz <rgplantz@outlook.com>
2023-03-25 10:45:22 -0700

    Update intToUDec.s

--- chapter_16/Text_Listings/add123_asm/intToUDec.s
+++ chapter_16/Text_Listings/add123_asm/intToUDec.s
@@ -9,35 +9,52 @@
         .equ    RADIX, 10             // number base
         .equ    INT2CHAR, 0x30        // ascii zero
 // Stack frame
-        .equ    frameSize, 32         // for reversed string
+// Stack frame
+        .equ    save1920, 16
+        .equ    save2122, 32
+        .equ    save23, 48
+        .equ    reverseDec, 56
+        .equ    frame, 80
 // Code
         .text
         .align  2
         .global intToUDec
         .type   intToUDec, %function
 intToUDec:
-        sub     sp, sp, frameSize     // place for local string
-        mov     w2, RADIX             // decimal
-        mov     w3, 0                 // count = 0
-        mov     x4, sp                // pointer to local string
-        strb    wzr, [x4]             // store NUL
-        add     x4, x4, 1             // and increment pointer
-doWhile:
-        udiv    w5, w1, w2            // compute quotient
-        msub    w6, w5, w2, w1        // w6 = quotient - RADIX * quotient
-        orr     w6, w6, INT2CHAR      // convert to ascii
-        strb    w6, [x4]              // store character
-        add     x4, x4, 1             // increment pointer
-        add     w3, w3, 1             // increment counter
-        mov     w1, w5                // remove remainder
-        cbnz    w1, doWhile           // continue if more left
-copyLoop:
-        sub     x4, x4, 1             // decrement from pointer
-        ldrb    w2, [x4]              // load character
-        strb    w2, [x0]              // store it
-        add     x0, x0, 1             // increment to pointer
-        cbnz    w2, copyLoop          // continue until NUL char
+        stp     fp, lr, [sp, -frame]! // create our stack frame
+        mov     fp, sp                // set our frame pointer
+        stp     x19, x20, [sp, save1920]  // save registers
+        stp     x21, x22, [sp, save2122]
+        str     x23, [sp, save23]
 
-        mov     w0, w3                // return count;
-        add     sp, sp, frameSize     // delete local storage
-        ret
+        mov     x19, x0               // place for result
+        mov     w20, w1               // value to convert
+        mov     w21, RADIX
+        add     x23, sp, reverseDec   // pointer to local string
+
+        strb    wzr, [x23]            // store NUL
+doWhile:
+        add     x23, x23, 1           // increment pointer
+        udiv    w0, w20, w21          // compute quotient
+        msub    w1, w0, w21, w20      // remainder = quotient - RADIX * quotient
+        orr     w1, w1, INT2CHAR      // convert to ascii
+        strb    w1, [x23]             // store character
+        mov     w20, w0               // remove remainder
+        cbnz    w20, doWhile          // continue if more left
+
+        mov     w22, wzr              // count = 0;
+copyLoop:
+        ldrb    w0, [x23]             // load character
+        strb    w0, [x19]             // store it
+        add     x19, x19, 1           // increment to pointer
+        sub     x23, x23, 1           // decrement from pointer
+        add     w22, w22, 1           // increment counter
+        cbnz    w0, copyLoop          // continue until NUL char
+        strb    w0, [x19]             // store NUL character
+
+        mov     w0, w22               // return count;
+        ldr     x23, [sp, save23]     // restore registers
+        ldp     x21, x22, [sp, save2122]
+        ldp     x19, x20, [sp, save1920]
+        ldp     fp, lr, [sp], frame   // restore fp, lr, sp
+        ret                           // back to caller
