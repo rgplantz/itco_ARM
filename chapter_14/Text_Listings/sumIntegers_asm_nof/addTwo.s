6f789d3ce15ec27ccc72768519084fd8f7b1038d
rgplantz <rgplantz@outlook.com>
2023-03-28 16:46:13 -0700

    Revert "Update addTwo.s"
    This reverts commit b5ceeca848c5cba162a56d41e3ac3b4d4684fcb3.

--- chapter_14/Text_Listings/sumIntegers_asm_nof/addTwo.s
+++ chapter_14/Text_Listings/sumIntegers_asm_nof/addTwo.s
@@ -1,3 +1,4 @@
+// addTwo.s
 // Adds two integers and outputs sum.
 // Calling sequence:
 //    w0 <- integer
@@ -6,7 +7,8 @@
 //    returns 0
         .arch armv8-a
 // Stack frame
-        .equ    frame, 16
+        .equ    regs, 16
+        .equ    frame,32
 // Code
         .text
         .align  2
@@ -15,10 +17,13 @@
 addTwo:
         stp     fp, lr, [sp, -frame]! // create our stack frame
         mov     fp, sp                // set our frame pointer
+        stp     x19, x20, [sp, regs]  // save for local vars
 
-        add     w0, w0, w1            // compute sum
-        str     w0, [x2]              // output sum
+        mov     x20, x2               // for output
+        add     w19, w0, w1           // compute sum
+        str     w19, [x20]            // output sum
 
         mov     w0, wzr               // return 0
+        ldp     x19, x20, [sp, regs]  // restore reg.
         ldp     fp, lr, [sp], frame   // restore fp, lr, sp
         ret                           // back to caller
