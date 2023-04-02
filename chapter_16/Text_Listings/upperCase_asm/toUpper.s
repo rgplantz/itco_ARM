7ca6382cb97c12b6e080013a682c78503865caf6
rgplantz <rgplantz@outlook.com>
2023-04-02 16:05:57 -0700

    Update toUpper.s

--- chapter_16/Text_Listings/upperCase_asm/toUpper.s
+++ chapter_16/Text_Listings/upperCase_asm/toUpper.s
@@ -12,18 +12,18 @@
         .global toUpper
         .type   toUpper, %function
 toUpper:
-        mov     w3, wzr               // counter = 0;
+        mov     w2, wzr               // counter = 0;
 convertLoop:
-        ldrb    w4, [x1]              // load character
-        cbz     w4, allDone           // all done if NUL char
-        movz    w5, UPMASK            // if not, do masking
-        and     w4, w4, w5            // mask to upper
-        strb    w4, [x0]              // store result
+        ldrb    w3, [x1]              // load character
+        cbz     w3, allDone           // all done if NUL char
+        movz    w4, UPMASK            // if not, do masking
+        and     w3, w3, w4            // mask to upper
+        strb    w3, [x0]              // store result
         add     x0, x0, 1             // increment destination pointer
         add     x1, x1, 1             //        source pointer
-        add     w3, w3, 1             //        and counter
+        add     w2, w2, 1             //        and counter
         b       convertLoop           // and continue
 allDone:
-        strb    w4, [x0]              // terminating NUL got us here
-        mov     w0, w3                // return counter
+        strb    w3, [x0]              // terminating NUL got us here
+        mov     w0, w2                // return counter
         ret                           // back to caller
