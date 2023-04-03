a8b3c4956b746fffd4c9d556893c59a6a2942327
rgplantz <rgplantz@outlook.com>
2023-04-03 16:23:30 -0700

    change to signed intToDec

--- chapter_16/Text_Listings/add123_C/intToDec.c
+++ chapter_16/Text_Listings/add123_C/intToDec.c
@@ -1,27 +1,37 @@
-// Converts an int to corresponding unsigned decimal text
+// Converts an int to corresponding decimal text
 // string representation. Returns number of characters.
 
-#include "uIntToDec.h"
+#include "intToDec.h"
+#inlucde <stdbool.h>
 #define ARRAYSZ 11
 #define ASCII 0x30
 #define RADIX 10
 #define NUL '\0'
 
-int uIntToDec(char *decString, unsigned int theInt) {
+int uIntToDec(char *decString, int theInt) {
     int count;
     char reverseDec[ARRAYSZ];
     char digit;
     char *ptr;
+    bool negative = false;
     
+    if (theInt < 0) {
+        negative = true;
+        theInt = -theInt;
+    }
     ptr = reverseDec;   // point to local char array
     *ptr = NUL;         // start with termination char
     do {
         ptr++;
-        digit = theInt % RADIX;
+        digit = (char)(theInt % RADIX);
         *ptr = ASCII | digit;
         theInt = theInt / RADIX;
     } while (theInt > 0);
 
+    if (negative) {
+        *ptr = "-";
+        ptr++;
+    }
     count = 0;          // reverse the string
     do {
         *decString = *ptr;
--- chapter_16/Text_Listings/add123_C/uIntToDec.h
+++
@@ -1,7 +0,0 @@
-// Converts an int to corresponding unsigned decimal text
-// string representation. Returns number of characters.
-
-#ifndef UINTTODEC_H
-#define UINTTODEC_H
-int uIntToDec(char *decString, unsigned int theInt);
-#endif
