62ccf6a7971972a1e87b196d2b70797c64de25cd
rgplantz <rgplantz@outlook.com>
2023-04-07 08:22:46 -0700

    Add sub123 to Your Turn chapter 16

---
+++ chapter_16/Your_Turn/sub123_C/sub123.c
@@ -0,0 +1,27 @@
+// Reads an int from user, subtracts 123,
+// and displays the result.
+
+#include "writeStr.h"
+#include "readStr.h"
+#include "decToInt.h"
+#include "intToDec.h"
+#define MAX 11
+#define ARRAYSZ MAX+1
+
+int main(void) {
+    char theString[ARRAYSZ];
+    int theInt;
+    
+    writeStr("Enter an integer: ");
+    readStr(theString, MAX);
+
+    decToInt(&theInt, theString);
+    theInt -= 123;
+    intToDec(theString, theInt);
+    
+    writeStr("The result is: ");
+    writeStr(theString);
+    writeStr("\n");
+    
+    return 0;
+}
