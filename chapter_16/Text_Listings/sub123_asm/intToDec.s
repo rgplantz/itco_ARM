f6b47d149e61576ee0d1441163fc0fe3fbc9c418
rgplantz <rgplantz@outlook.com>
2023-04-09 12:19:59 -0700

    Update intToDec.s

--- chapter_16/Text_Listings/sub123_asm/intToDec.s
+++ chapter_16/Text_Listings/sub123_asm/intToDec.s
@@ -21,36 +21,35 @@
 intToDec:
         sub     sp, sp, frame         // local string on stack
 
-        mov     w2, wzr               // count = 0
         cmp     w1, wzr               // => 0?
         tbz     w1, 31, nonNegative   // yes, go to conversion
         neg     w1, w1                // no, negate int
-        mov     w3, MINUS
-        strb    w3, [x0]              // start with minus sign
+        mov     w2, MINUS
+        strb    w2, [x0]              // start with minus sign
         add     x0, x0, 1             // increment pointer
-        add     w2, w2, 1             // increment counter
 nonNegative:
         add     x3, sp, reverseDec    // pointer to local string storage
         strb    wzr, [x3]             // start with NUL
-        mov     w4, RADIX             // put in register
+        mov     w3, RADIX             // put in register
 doWhile:
         add     x3, x3, 1             // increment local pointer
-        udiv    w5, w1, w4            // compute quotient
-        msub    w6, w5, w4, w1        // remainder = quotient - RADIX * quotient
-        orr     w6, w6, INT2CHAR      // convert to ascii
-        strb    w6, [x3]              // store character
-        mov     w1, w5                // remove remainder
+        udiv    w4, w1, w3            // compute quotient
+        msub    w5, w4, w3, w1        // remainder = quotient - RADIX * quotient
+        orr     w5, w5, INT2CHAR      // convert to ascii
+        strb    w5, [x3]              // store character
+        mov     w1, w4                // remove remainder
         cbnz    w1, doWhile           // continue if more left
 
+        mov     w6, wzr               // count = 0
 copyLoop:
-        ldrb    w6, [x3]              // load character
-        strb    w6, [x0]              // store it
+        ldrb    w5, [x3]              // load character
+        strb    w5, [x0]              // store it
         add     x0, x0, 1             // increment to pointer
         sub     x3, x3, 1             // decrement from pointer
-        add     w2, w2, 1             // increment counter
-        cbnz    w6, copyLoop          // continue until NUL char
-        strb    w6, [x0]              // store NUL character
+        add     w6, w6, 1             // increment counter
+        cbnz    w5, copyLoop          // continue until NUL char
+        strb    w5, [x0]              // store NUL character
 
-        mov     w0, w2                // return count;
+        mov     w0, w6                // return count;
         add     sp, sp, frame         // restore sp
         ret                           // back to caller
