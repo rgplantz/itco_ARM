654808899f8e6b94241685718e3441af2ddb0475
rgplantz <rgplantz@outlook.com>
2023-04-15 08:43:06 -0700

    Add header files for putInt and getInt

---
+++ chapter_16/Your_Turn/arithmetic_asm/getInt.h
@@ -0,0 +1,6 @@
+// Reads a decimal integer from keyboard and converts to an int.
+
+#ifndef GETINTH
+#define GETINTH
+int getInt(void);
+#endif
---
+++ chapter_16/Your_Turn/arithmetic_asm/putInt.h
@@ -0,0 +1,6 @@
+// Displays an int on screen.
+
+#ifndef PUTINTH
+#define PUTINTH
+int putInt(int);
+#endif
