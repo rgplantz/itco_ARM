68689a6d6f6dc4c69c595a33aa0e59bfbaf1b481
rgplantz <rgplantz@outlook.com>
2023-04-18 11:14:56 -0700

    Update displayArray.s

--- chapter_17/Text_Listings/arrayPass_asm/displayArray.s
+++ chapter_17/Text_Listings/arrayPass_asm/displayArray.s
@@ -36,7 +36,7 @@ displayLoop:
         bl      putInt
         adr     x0, msg2              // more text on line
         bl      writeStr
-        ldr     x0, [x0, w2, uxtw 2]  // current element
+        ldr     w0, [x19, w21, uxtw 2]  // current element
         bl      putInt
         adr     x0, newline           // finish line
         bl      writeStr
