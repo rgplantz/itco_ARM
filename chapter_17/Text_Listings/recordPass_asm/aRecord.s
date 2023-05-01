476f50838d1900ea5272d1aca12ed12acd803f31
rgplantz <rgplantz@outlook.com>
2023-05-01 15:01:30 -0700

    Add aRecord.s

---
+++ chapter_17/Text_Listings/recordPass_asm/aRecord.s
@@ -0,0 +1,8 @@
+// Assembly language declaration of a record.
+// This record takes 12 bytes.
+        .equ    aChar, 0
+        .equ    bInt, 1
+        .equ    anInt, 2
+        .equ    oneInt, 4
+        .equ    twoInt, 8
+        
