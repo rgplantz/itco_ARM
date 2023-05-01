dad970d3e7e5668b7bb3bd79d3e752d4edabbbfe
rgplantz <rgplantz@outlook.com>
2023-05-01 15:29:35 -0700

    Update records.s

--- chapter_17/Text_Listings/recordPass_asm/records.s
+++ chapter_17/Text_Listings/recordPass_asm/records.s
@@ -17,8 +17,19 @@ main:
         stp     fp, lr, [sp, -frame]!   // create our stack frame
         mov     fp, sp                  // Set frame pointer
 
+        mov     w5, 'c'                 // data to load
+        mov     w4, 34
+        mov     w3, 'b'
+        mov     w2, 12
+        mov     w1, 'a' 
         add     x0, sp, xRecord         // address of first record
         bl      loadRecord              // load values
+
+        mov     w5, 'f'                 // data to load
+        mov     w4, 78
+        mov     w3, 'e'
+        mov     w2, 56
+        mov     w1, 'd' 
         add     x0, sp, yRecord         // address of second record
         bl      loadRecord              // load values
 
