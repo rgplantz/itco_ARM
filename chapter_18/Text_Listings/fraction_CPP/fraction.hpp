d0795ac46a5df26e7e094647032a291efce47a17
rgplantz <rgplantz@outlook.com>
2023-05-30 11:35:25 -0700

    Update fraction.hpp

--- chapter_18/Text_Listings/fraction_CPP/fraction.hpp
+++ chapter_18/Text_Listings/fraction_CPP/fraction.hpp
@@ -3,13 +3,13 @@
 #ifndef FRACTION_H
 #define FRACTION_H
 class fraction {
-        int num {0};
+        int num {0};              // specify defaults               
         int den {1};
     public:
-        fraction();                 // default constructor
-        ~fraction();                // destructor
-        void get();                 // gets user's values
-        void display();             // displays fraction
-        void add(int theValue);     // adds theValue
+        fraction();               // default constructor
+        ~fraction();              // destructor
+        void get();               // gets user's values
+        void display();           // displays fraction
+        void add(int theValue);   // adds theValue
 };
 #endif
