7e4348afbeae228373a715eeffd45538030445d5
rgplantz <rgplantz@outlook.com>
2023-05-31 11:05:42 -0700

    Update fractionNC.hpp

--- chapter_18/Text_Listings/fraction_CPP_noconstruct/fractionNC.hpp
+++ chapter_18/Text_Listings/fraction_CPP_noconstruct/fractionNC.hpp
@@ -6,8 +6,8 @@ class fraction {
         int num {0};              // specify defaults
         int den {1};
     public:
-        fraction() = default;     // use default values
-        ~fraction() = default;
+        fraction() = default;     // tell compiler to generate default
+        ~fraction() = default;    //     constructor and destructor
         void get();               // gets user's values
         void display();           // displays fraction
         void add(int theValue);   // adds theValue
