547e0cd7f1c78d691e9b428804485e4ad39d3903
rgplantz <rgplantz@outlook.com>
2023-05-30 20:33:50 -0700

    Update fractionNC.hpp

--- chapter_18/Text_Listings/fraction_CPP_noconstruct/fractionNC.hpp
+++ chapter_18/Text_Listings/fraction_CPP_noconstruct/fractionNC.hpp
@@ -7,6 +7,7 @@ class fraction {
         int den {1};
     public:
         fraction() = default;     // use default values
+        ~fraction() = default;
         void get();               // gets user's values
         void display();           // displays fraction
         void add(int theValue);   // adds theValue
