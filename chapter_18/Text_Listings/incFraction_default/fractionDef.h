a08ed1de7af21a7bdcdb18ba383763d8a837e97e
rgplantz <rgplantz@outlook.com>
2023-06-04 20:40:45 -0700

    Update fractionDef.h

--- chapter_18/Text_Listings/incFraction_default/fractionDef.h
+++ chapter_18/Text_Listings/incFraction_default/fractionDef.h
@@ -2,10 +2,10 @@
 
 #ifndef FRACTIONDEF_H
 #define FRACTIONDEF_H
-class fraction {
+class Fraction {
 public:
-    fraction() = default;     // tell compiler to generate default
-    ~fraction() = default;    //     constructor and destructor
+    Fraction() = default;     // tell compiler to generate default
+    ~Fraction() = default;    //     constructor and destructor
     void get();               // gets user's values
     void display();           // displays fraction
     void add_integer(int x);  // adds x to fraction
