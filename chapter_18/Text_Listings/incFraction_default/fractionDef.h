fcc5de690425784a420a8fadf6d892693467fa97
rgplantz <rgplantz@outlook.com>
2023-06-04 20:50:35 -0700

    Update fractionDef.h

--- chapter_18/Text_Listings/incFraction_default/fractionDef.h
+++ chapter_18/Text_Listings/incFraction_default/fractionDef.h
@@ -10,7 +10,7 @@ public:
     void display();           // displays fraction
     void add_integer(int x);  // adds x to fraction
 private:
-    int numerator; 
-    int denominator; 
+    int numerator {}; 
+    int denominator {1}; 
 };
 #endif
