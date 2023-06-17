aca89a3cb5f149b385371168fcf449e0e681292d
rgplantz <rgplantz@outlook.com>
2023-06-17 15:49:48 -0700

    Create fraction.h

---
+++ chapter_18/Text_Listings/inc_fraction/fraction.h
@@ -0,0 +1,16 @@
+// simple fraction class
+
+#ifndef FRACTION_H
+#define FRACTION_H
+class Fraction {
+public:
+    Fraction();               // default constructor
+    ~Fraction();              // destructor
+    void get();               // gets user's values
+    void display();           // displays fraction
+    void add_integer(int x);  // adds x to fraction
+private:
+    int numerator; 
+    int denominator; 
+};
+#endif
