3f70cda78d60a4284f3b71c9ec14607adf254509
rgplantz <rgplantz@outlook.com>
2023-07-02 12:52:52 -0700

    Change names

--- chapter_16/Text_Listings/convert_dec_asm/convert_dec.s
+++ chapter_16/Text_Listings/convert_dec_asm/convert_dec.s
@@ -4,7 +4,7 @@
         .equ    MAX, 11                   // character storage limit
 // Stack frame
         .equ    theInt, 16
-        .equ    theString, 20
+        .equ    the_string, 20
         .equ    frameSize, 32
 // Code
         .text
@@ -23,18 +23,18 @@ main:
         stp     fp, lr, [sp, -frameSize]! // create stack frame
         mov     fp, sp                    // our frame pointer
         adr     x0, prompt                // prompt message
-        bl      writeStr                  // ask for input
+        bl      write_str                 // ask for input
 
         mov     w1, MAX                   // limit number of input chars
-        add     x0, sp, theString         // place to store string
-        bl      readStr                    // get from keyboard
+        add     x0, sp, the_string        // place to store string
+        bl      read_str                  // get from keyboard
 
-        add     x1, sp, theString         // address of string
+        add     x1, sp, the_string        // address of string
         add     x0, sp, theInt            // place to store the int
-        bl      decToInt                 // do conversion
+        bl      dec_to_int                // do conversion
 
         ldr     w2, [sp, theInt]          // load the int
-        add     x1, sp, theString         // input text string
+        add     x1, sp, the_string        // input text string
         adr     x0, format                // format message
         bl      printf                    // show results
         mov     w0, 0                     // return 0;
--- chapter_16/Text_Listings/convert_dec_asm/dec_to_int.s
+++ chapter_16/Text_Listings/convert_dec_asm/dec_to_int.s
@@ -10,9 +10,9 @@
 // Program code
         .text
         .align  2
-        .global decToInt
-        .type   decToInt, %function
-decToInt:
+        .global dec_to_int
+        .type   dec_to_int, %function
+dec_to_int:
         mov     w2, wzr               // count = 0;
         mov     w3, wzr               // result = 0;
         mov     w4, wzr               // negative = false;
@@ -20,25 +20,25 @@ decToInt:
 
         ldrb    w6, [x1]              // load first character
         cmp     w6, '-                // minus sign?
-        b.ne    checkPos              // no, check for plus sign
+        b.ne    check_pos             // no, check for plus sign
         mov     x4, 1                 // yes, negative = true;
         add     x1, x1, 1             // increment string pointer
-        b       convertLoop           // and convert numerals
-checkPos:
+        b       convert               // and convert numerals
+check_pos:
         cmp     w6, '+                // plus sign?
-        b.ne    convertLoop           // no, convert numerals
+        b.ne    convert               // no, convert numerals
         add     x1, x1, 1             // yes, skip over it
 
-convertLoop:
+convert:
         ldrb    w6, [x1]              // load character
-        cbz     w6, checkSign         // NUL char?
+        cbz     w6, check_sign        // NUL char?
         and     w6, w6, INTMASK       // no, mask to integer
         mul     w3, w3, w5            // result times RADIX
         add     w3, w3, w6            // add new integer
         add     w2, w2, 1             // count++;
         add     x1, x1, 1             // stringPtr++;
-        b       convertLoop           // and continue
-checkSign:
+        b       convert               // and continue
+check_sign:
         cbz     w4, positive          // check negative flag
         neg     w3, w3                // negate if flag is true
 positive:
