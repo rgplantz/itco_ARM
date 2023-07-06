eff16fd40b97061b927f737929d67f640dc08ace
rgplantz <rgplantz@outlook.com>
2023-07-06 11:47:44 -0700

    Update convert_dec.s

--- chapter_16/text_listings/convert_dec_asm/convert_dec.s
+++ chapter_16/text_listings/convert_dec_asm/convert_dec.s
@@ -1,11 +1,11 @@
 // Gets decimal number from user and stores it as an int
         .arch armv8-a
 // Useful constant
-        .equ    MAX, 11                   // character storage limit
+        .equ    MAX, 11               // character storage limit
 // Stack frame
-        .equ    theInt, 16
+        .equ    the_int, 16
         .equ    the_string, 20
-        .equ    frameSize, 32
+        .equ    frame, 32
 // Code
         .text
         .section        .rodata
@@ -20,23 +20,23 @@ format:
         .global main
         .type   main, %function
 main:
-        stp     fp, lr, [sp, -frameSize]! // create stack frame
-        mov     fp, sp                    // our frame pointer
-        adr     x0, prompt                // prompt message
-        bl      write_str                 // ask for input
+        stp     fp, lr, [sp, -frame]! // create stack frame
+        mov     fp, sp                // our frame pointer
+        adr     x0, prompt            // prompt message
+        bl      write_str             // ask for input
 
-        mov     w1, MAX                   // limit number of input chars
-        add     x0, sp, the_string        // place to store string
-        bl      read_str                  // get from keyboard
+        mov     w1, MAX               // limit number of input chars
+        add     x0, sp, the_string    // place to store string
+        bl      read_str              // get from keyboard
 
-        add     x1, sp, the_string        // address of string
-        add     x0, sp, theInt            // place to store the int
-        bl      dec_to_int                // do conversion
+        add     x1, sp, the_string    // address of string
+        add     x0, sp, the_int       // place to store the int
+        bl      dec_to_int            // do conversion
 
-        ldr     w2, [sp, theInt]          // load the int
-        add     x1, sp, the_string        // input text string
-        adr     x0, format                // format message
-        bl      printf                    // show results
-        mov     w0, 0                     // return 0;
-        ldp     x29, x30, [sp], frameSize // undo stack frame
+        ldr     w2, [sp, the_int]     // load the int
+        add     x1, sp, the_string    // input text string
+        adr     x0, format            // format message
+        bl      printf                // show results
+        mov     w0, 0                 // return 0;
+        ldp     x29, x30, [sp], frame // undo stack frame
         ret
