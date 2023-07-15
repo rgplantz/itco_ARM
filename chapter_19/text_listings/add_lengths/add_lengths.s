ce37515cb86bf407368f0e67b789de3a7d659aff
rgplantz <rgplantz@outlook.com>
2023-07-15 16:06:13 -0700

    Update add_lengths.s

--- chapter_19/text_listings/add_lengths/add_lengths.s
+++ chapter_19/text_listings/add_lengths/add_lengths.s
@@ -1,8 +1,7 @@
-// Adds 2 lengthx, fixed-point to nearest sixteenth
+// Adds 2 lengths, fixed-point to nearest sixteenth
         .arch armv8-a
 // Stack frame
-        .equ    x, 16
-        .equ    y, 20
+        .equ    save1920, 16
         .equ    frame, 32
 // Constant data
         .section	.rodata
@@ -17,21 +16,21 @@ sum_msg:
 main:
         stp     fp, lr, [sp, -frame]! // create our stack frame
         mov     fp, sp                // set our frame pointer
+        stp     x19, x20, [sp, save1920]  // for local vars
 
         bl      get_length
-        str     w0, [sp, x]           // first number
+        mov     w19, w0               // first number
         bl      get_length
-        str     w0, [sp, y]           // second number
+        mov     w20, w0               // second number
 
         adr     x0, sum_msg           // some formatting
         bl      write_str
-        ldr     w0, [sp, x]           // add x and y
-        ldr     w1, [sp, y]
-        add     w0, w0, w1
+        add     w0, w20, w19          // add lengths
         bl      display_length        // show result
         mov     w0, '\n'              // finish formatting
         bl      write_char
 
         mov     w0, wzr               // return 0;
+        ldp     x19, x20, [sp, save1920]  // restore for caller
         ldp     fp, lr, [sp], frame   // restore fp, lr, sp
         ret                           // back to caller
