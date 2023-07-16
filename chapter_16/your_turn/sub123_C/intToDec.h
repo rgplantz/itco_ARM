ad91ec2af0c15539aa8788a7573bc186f8d23951
rgplantz <rgplantz@outlook.com>
2023-07-16 11:17:38 -0700

    Delete C sub 123 program

--- chapter_16/your_turn/sub123_C/intToDec.h
+++
@@ -1,7 +0,0 @@
-// Converts an int to corresponding decimal text
-// string representation. Returns number of characters.
-
-#ifndef INTTODEC_H
-#define INTTODEC_H
-int intToDec(char *decString, int theInt);
-#endif
