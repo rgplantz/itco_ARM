4a595c818f69d9f1369d886c25fe9cab00019423
rgplantz <rgplantz@outlook.com>
2023-09-29 16:12:11 -0700

    to snake case

--- chapter_13/text_listings/coin_flips2_asm/coin_flips2.s
+++ chapter_13/text_listings/coin_flips2_asm/coin_flips2.s
@@ -1,17 +1,16 @@
-// coinFlips2.s
 // Flips a coin, heads or tails.
         .arch armv8-a
 // Useful names
-        .equ    NTIMES,10             // number of flips
+        .equ    N_TIMES,              // number of flips
         .equ    RAND_MID,1073741823   // RAND_MAX/2
 // Stack frame
-        .equ    save19, 24
-        .equ    frame,32
+        .equ    SAVE19, 24
+        .equ    FRAME,32
 // Constant data
         .section        .rodata
-headsMsg:
+heads_msg:
         .string "heads"
-tailsMsg:
+tails_msg:
         .string "tails"
 // Code
         .text
@@ -19,29 +18,29 @@ tailsMsg:
         .global main
         .type   main, %function
 main:
-        stp     fp, lr, [sp, -frame]! // create our stack frame
+        stp     fp, lr, [sp, -FRAME]! // create our stack frame
         mov     fp, sp                // set our frame pointer
-        str     x19, [sp, save19]     // save for i local var.
+        str     x19, [sp, SAVE19]     // save for i local var.
         mov     w19, wzr              // i = 0;
-forLoop:
-        mov     w0, NTIMES            // total number of times
+for_loop:
+        mov     w0, N_TIMES           // total number of times
         cmp     w19, w0               // is i at end?
-        b.hs    allDone               // yes
+        b.hs    done                  // yes
         bl      random                // no, get random number
         mov     w1, RAND_MID          // half way
         cmp     w1, w0                // above or below middle?
         b.hi    tails                 // above -> tails
-        adr     x0, headsMsg          // below -> heads message
+        adr     x0, heads_msg         // below -> heads message
         bl      puts                  // print message
         b       continue              // skip else part
 tails:
-        adr     x0, tailsMsg          // tails message page address
+        adr     x0, tails_msg         // tails message page address
         bl      puts                  // print message
 continue:
         add     w19, w19, 1           // i++;
-        b       forLoop               // and continue loop
-allDone:
+        b       for_foop              // and continue loop
+done:
         mov     w0, wzr               // return 0
-        ldr     x19, [sp, save19]     // restore reg.
-        ldp     fp, lr, [sp], frame   // restore fp, lr, sp
+        ldr     x19, [sp, SAVE19]     // restore reg.
+        ldp     fp, lr, [sp], FRAME   // restore fp, lr, sp
         ret                           // back to caller
