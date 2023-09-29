09b218d7fd99f9b86b9e0e42cf906f61d2d3c9bc
rgplantz <rgplantz@outlook.com>
2023-09-29 16:04:58 -0700

    to snake case

---
+++ chapter_13/text_listings/coin_flips1_C/coin_flips1.c
@@ -0,0 +1,20 @@
+// Flips a coin, shows heads.
+
+#include <stdio.h>
+#include <stdlib.h>
+#define N_TIMES 10
+
+int main()
+{
+    register int random_number;
+    register int i;
+
+    for (i = 0; i < N_TIMES; i++) {
+        random_number = random();
+        if (random_number < RAND_MAX/2) {
+            puts("heads");
+        }
+    }
+
+    return 0;
+}
