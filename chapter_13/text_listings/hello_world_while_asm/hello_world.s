5cf8d5f7bcce6b728c6c27e183e7f83d744ea50d
rgplantz <rgplantz@outlook.com>
2023-09-30 20:49:21 -0700

    to snake case

--- chapter_13/text_listings/hello_world_while_asm/hello_world.s
+++ chapter_13/text_listings/hello_world_while_asm/hello_world.s
@@ -1,4 +1,3 @@
-// helloWorld.s
 // Writes Hello, World! one character at a time
         .arch armv8-a
 // Useful names
@@ -7,7 +6,7 @@
 // Stack frame
         .equ    FRAME,16
 // Constant data
-        .section        .rodata
+        .section  .rodata
 message:
         .string "Hello, World!\n"
 // Code
@@ -19,17 +18,17 @@ main:
         stp     fp, lr, [sp, -FRAME]! // create our stack frame
         mov     fp, sp                // set our frame pointer
         adr     x19, message          // address of message
-whileLoop:
+loop:
         ldrb    w0, [x19]             // load character
         cmp     w0, NUL               // end of string?
-        b.eq    allDone               // yes
+        b.eq    done                  // yes
         mov     w2, 1                 // no, one char
         mov     x1, x19               // address of char
         mov     x0, STDOUT            // write on screen
         bl      write
         add     x19, x19, 1           // increment pointer
-        b       whileLoop             // and continue
-allDone:
+        b       loop                  // and continue
+done:
         mov     w0, wzr               // return value
         ldp     fp, lr, [sp], FRAME   // undo our stack frame
         ret
