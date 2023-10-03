0065110470f2b160c84dfcea6a84aae074f6f45c
rgplantz <rgplantz@outlook.com>
2023-10-03 08:29:19 -0700

    fix coin_flips2.c

--- chapter_13/text_listings/coin_flips2_C/coin_flips2.c
+++ chapter_13/text_listings/coin_flips2_C/coin_flips2.c
@@ -9,7 +9,7 @@ int main()
     register int random_number;
     register int i;
 
-    for (i = 0; i < 10N_TIMES; i++) {
+    for (i = 0; i < N_TIMES; i++) {
         random_number = random();
         if (random_number < RAND_MAX/2) {
             puts("heads");
