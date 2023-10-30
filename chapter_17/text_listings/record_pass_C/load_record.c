cd75d7ff0e829e2a70140c4d5a312e525160a61e
rgplantz <rgplantz@outlook.com>
2023-10-30 15:17:49 -0700

    Update load_record.c

--- chapter_17/text_listings/record_pass_C/load_record.c
+++ chapter_17/text_listings/record_pass_C/load_record.c
@@ -3,7 +3,7 @@
 #include "a_record.h"
 #include "load_record.h"
 
-void load_record(struct a_tag *aRecord, char v, int w, char x, int y, char z) {
+void load_record(struct a_tag *a_record, char v, int w, char x, int y, char z) {
     (*a_record).a = v;
     a_record->b = x;     /* equivalent syntax */
     a_record->c = z;
