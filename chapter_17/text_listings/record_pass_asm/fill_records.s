78049403262eea93c265da4696c8fa53fc8d86fc
rgplantz <rgplantz@outlook.com>
2023-10-30 14:55:51 -0700

    Update fill_records.s

--- chapter_17/text_listings/record_pass_asm/fill_records.s
+++ chapter_17/text_listings/record_pass_asm/fill_records.s
@@ -4,14 +4,14 @@
 // Stack frame
         .equ    x, 16
         .equ    y, 32
-        .equ    frame, 48
+        .equ    FRAME, 48
 // Code
         .text
         .align  2
         .global main
         .type   main, %function
 main:
-        stp     fp, lr, [sp, -frame]!   // create our stack frame
+        stp     fp, lr, [sp, -FRAME]!   // create our stack frame
         mov     fp, sp                  // Set frame pointer
 
         mov     w5, 'c'                 // data to load
@@ -20,7 +20,7 @@ main:
         mov     w2, 12
         mov     w1, 'a' 
         add     x0, sp, x               // address of first record
-        bl      loadRecord              // load values
+        bl      load_record             // load values
 
         mov     w5, 'f'                 // data to load
         mov     w4, 78
@@ -28,16 +28,16 @@ main:
         mov     w2, 56
         mov     w1, 'd' 
         add     x0, sp, y               // address of second record
-        bl      loadRecord              // load values
+        bl      load_record             // load values
 
         ldr     x0, [sp, x]             // 1st 8 bytes of x
         ldr     w1, [sp, x+8]           // last 4 bytes of x
-        bl      displayRecord           // display x
+        bl      display_record          // display x
 
         ldr     x0, [sp, y]             // 1st 8 bytes of y
         ldr     w1, [sp, y+8]           // last 4 bytes of y
-        bl      displayRecord           // display y
+        bl      display_record          // display y
 
         mov     w0, wzr                 // return 0;
-        ldp     fp, lr, [sp], frame     // undo stack frame
+        ldp     fp, lr, [sp], FRAME     // undo stack frame
         ret
