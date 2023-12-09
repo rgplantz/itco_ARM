6126f0e3cce1cc5f39267d69c1d19e02a99f3c3b
rgplantz <rgplantz@outlook.com>
2023-12-09 15:53:31 -0800

    Update UART_defs.h

--- chapter_20/text_listings/UART_echo_C/UART_defs.h
+++ chapter_20/text_listings/UART_echo_C/UART_defs.h
@@ -20,13 +20,13 @@
 #define TxRDY 0x20  // transmitter ready
 
 // commands
-#define NO_FIFO        0x00   // don't use FIFO
-#define NO_INTERRUPT   0x00   // polling mode
-#define MSB_38400      0x00   // 2 bytes used to
-#define LSB_38400      0x03   // set baud 38400
-#define N_BITS         0x03   // 8 bits
-#define STOP_BIT       0x00   // 1 stop bit
-#define NO_PARITY      0x00
-#define SET_COM        N_BITS | STOP_BIT | NO_PARITY
-#define SET_BAUD       0x80 | SET_COM
+#define NO_FIFO       0x00  // don't use FIFO
+#define NO_INTERRUPT  0x00  // polling mode
+#define MSB_38400     0x00  // 2 bytes used to
+#define LSB_38400     0x03  // set baud 38400
+#define N_BITS        0x03  // 8 bits
+#define STOP_BIT      0x00  // 1 stop bit
+#define NO_PARITY     0x00
+#define SET_COM       N_BITS | STOP_BIT | NO_PARITY
+#define SET_BAUD      0x80 | SET_COM
 #endif
