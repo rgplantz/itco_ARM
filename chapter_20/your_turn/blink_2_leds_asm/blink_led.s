b5611f3b62f54d883addd0ed2bb968ba0bd36d06
rgplantz <rgplantz@outlook.com>
2023-12-17 16:01:11 -0800

    Add two LED program

--- chapter_20/your_turn/blink_2_leds_asm/blink_led.s
+++ chapter_20/your_turn/blink_2_leds_asm/blink_led.s
@@ -1,11 +1,12 @@
-// Blinks LED connected between GPIO pin 17 and ground on
-// once once every three seconds.
+// Alternately blinks LEDs connected between GPIO pins 16 & 17
+// once once every three three seconds.
 
 // Useful constants
         .equ    N_BLINKS, 5           // number of times to blink
         .equ    DELTA_TIME, 3         // seconds between blinks
         .equ    OUTPUT, 1             // use pin for output
-        .equ    GPIO_PIN, 17          // bit field in GPFLSEL regs.
+        .equ    GPIO_PIN_1, 16        // bit field in GPFLSEL regs.
+        .equ    GPIO_PIN_2, 17        // bit field in GPFLSEL regs.
 
 // Stack frame
         .equ    save1920, 16          // save regs
@@ -15,10 +16,10 @@
         .align  2
 err_msg:
         .asciz  "Cannot map /dev/gpiomem\n"
-on_msg:
-        .asciz  "led on...\n"
-off_msg:
-        .asciz  "...led off\n"
+on_1_msg:
+        .asciz  "led 1 on...\n"
+on_2_msg:
+        .asciz  "led 2 on...\n"
 
 // Code
         .text
@@ -38,33 +39,46 @@ main:
         b       done
 mem_map_ok:
         mov     x19, x0                 // pointer to mapped memory
-// Select pin to be an output
+// Select pins to be an outputs
         mov     w2, OUTPUT              // make GPIO pin an output
-        mov     w1, GPIO_PIN            // GPIO pin number
+        mov     w1, GPIO_PIN_1          // GPIO pin number
+        bl      gpio_pin_function
+        mov     w2, OUTPUT              // make GPIO pin an output
+        mov     w1, GPIO_PIN_2          // GPIO pin number
         bl      gpio_pin_function
 
 // Turn the pin on and off
+        adr     x0, on_1_msg            // tell user LED 1 is on
+        bl      write_str
+        mov     w1, GPIO_PIN_1          // LED 1 pin number
+        mov     x0, x19                 // pointer to mapped memory
+        bl      gpio_pin_set            // turn LED 1 on
+
         mov     x20, N_BLINKS           // number of times to do it
 loop: 
-        adr     x0, on_msg              // tell user it's on
-        bl      write_str
-        mov     w1, GPIO_PIN            // GPIO pin number
+        mov     w1, GPIO_PIN_2          // LED 2 pin number
         mov     x0, x19                 // pointer to mapped memory
-        bl      gpio_pin_set            // turn LED on
+        bl      gpio_pin_clr            // turn LED 2 off
         mov     w0, DELTA_TIME          // wait
         bl      sleep
-       
-        adr     x0, off_msg             // tell user it's off
+
+        adr     x0, on_2_msg            // tell user LED 2 is on
         bl      write_str
-        mov     w1, GPIO_PIN            // GPIO pin number
+        mov     w1, GPIO_PIN_2          // LED 2 on
         mov     x0, x19                 // pointer to mapped memory
-        bl      gpio_pin_clr            // turn LED off
+        bl      gpio_pin_set            // turn LED on
+        mov     w1, GPIO_PIN_1          // LED 1 pin number
+        mov     x0, x19                 // pointer to mapped memory
+        bl      gpio_pin_clr            // turn LED 1 off
         mov     w0, DELTA_TIME          // wait
         bl      sleep
 
         sub     x20, x20, 1             // decrement loop counter
         cbnz    x20, loop               // loop if not done
-       
+
+        mov     w1, GPIO_PIN_2          // LED 2 pin number
+        mov     x0, x19                 // pointer to mapped memory
+        bl      gpio_pin_clr            // turn LED 2 off
 done:
         mov     x0, x19                 // our gpio memory
         bl      gpio_unmap_memory
---
+++ chapter_20/your_turn/blink_2_leds_asm/gpio_pin_set.s
@@ -0,0 +1,21 @@
+// Sets GPIO pin. Assumes that GPIO registers
+// have been mapped to programming memory.
+// Calling sequence:
+//       x0 <- address of GPIO in mapped memory
+//       w1 <- pin number
+
+// Constants
+        .equ    GPSET0, 0x1c      // GPSET register offset
+
+// Code
+        .text
+        .align  2
+        .global gpio_pin_set
+        .type   gpio_pin_set, %function
+gpio_pin_set:
+        add     x0, x0, GPSET0  // address of GPSET0 register
+        mov     w2, 1           // need a 1
+        lsl     w2, w2, w1      // move to specified bit position
+        str     w2, [x0]        // output
+        
+        ret
