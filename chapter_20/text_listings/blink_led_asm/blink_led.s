534e5ce299d382159822f16466314b2478a3ff7f
rgplantz <rgplantz@outlook.com>
2023-12-17 16:49:21 -0800

    Update blink_led.s

--- chapter_20/text_listings/blink_led_asm/blink_led.s
+++ chapter_20/text_listings/blink_led_asm/blink_led.s
@@ -62,8 +62,8 @@ loop:
         mov     w0, DELTA_TIME          // wait
         bl      sleep
 
-        sub     x20, x20, 1             // decrement loop counter
-        cbnz    x20, loop               // loop if not done
+        subs    x20, x20, 1             // decrement loop counter
+        b.ne    x20, loop               // loop if not zero
        
 done:
         mov     x0, x19                 // our gpio memory
