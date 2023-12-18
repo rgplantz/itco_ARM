2a8799fb83c52f4242c8660e645ec3a1d70257dd
rgplantz <rgplantz@outlook.com>
2023-12-18 13:16:18 -0800

    Update blink_led.s

--- chapter_20/text_listings/blink_led_asm/blink_led.s
+++ chapter_20/text_listings/blink_led_asm/blink_led.s
@@ -4,7 +4,7 @@
         .equ    N_BLINKS, 5           // number of times to blink
         .equ    DELTA_TIME, 3         // seconds between blinks
         .equ    OUTPUT, 1             // use pin for output
-        .equ    GPIO_PIN, 17          // bit field in GPFLSEL regs.
+        .equ    GPIO_PIN, 17          // pin number
 
 // Stack frame
         .equ    save1920, 16          // save regs
