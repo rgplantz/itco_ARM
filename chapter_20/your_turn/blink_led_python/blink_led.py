272e4f7747487088b5d23c0c6a4a3723b39012b5
rgplantz <rgplantz@outlook.com>
2023-12-23 14:31:18 -0800

    Update blink_led.py

--- chapter_20/your_turn/blink_led_python/blink_led.py
+++ chapter_20/your_turn/blink_led_python/blink_led.py
@@ -1,13 +1,14 @@
 # Blinks LED connected to GPIO pin 17
-from gpiozero import LED
+import pigpio
 from time import sleep
 
-led = LED(17)
+led = pigpio.pi()
+led.set_mode(17, pigpio.OUTPUT)
 
 for _ in range(5):
     print("led on...")
-    led.on()
+    led.write(17, 1)
     sleep(3)
     print("...led off")
-    led.off()
+    led.write(17, 0)
     sleep(3)
