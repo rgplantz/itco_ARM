d21198f4b9233a88bbfa7ee2f0a3cd20a8c9e358
rgplantz <rgplantz@outlook.com>
2024-01-22 11:24:45 -0800

    added your_turn chap 21

--- chapter_21/your_turn/echo_string/write_str.h
+++
@@ -1,9 +0,0 @@
-// Writes a C-style text string to the standard output (screen).
-// Calling sequence:
-//       x0 <- address of string to be written
-// returns number of characters written
-
-#ifndef WRITE_STR_H
-#define WRITE_STR_H
-int write_str(char *);
-#endif
