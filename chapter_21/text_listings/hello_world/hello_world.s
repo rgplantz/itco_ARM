6db7f860b3e40082f229bffa545b7ade977aa5dc
rgplantz <rgplantz@outlook.com>
2024-01-21 20:39:36 -0800

    Update hello_world.s

--- chapter_21/text_listings/hello_world/hello_world.s
+++ chapter_21/text_listings/hello_world/hello_world.s
@@ -1,10 +1,12 @@
-// Writes Hello, World! one character at a time
+// Writes Hello, World! using system call.
         .arch armv8-a
 // Useful names
         .equ    NUL, 0
         .equ    STDOUT, 1
+        .equ    WRITE, 0
 // Stack frame
-        .equ    FRAME,16
+        .equ    save19, 16
+        .equ    FRAME, 32
 // Constant data
         .section  .rodata
 message:
@@ -16,6 +18,7 @@ message:
         .type   main, %function
 main:
         stp     fp, lr, [sp, -FRAME]! // create our stack frame
+        str     x19, [sp, save19]     // save reg
         mov     fp, sp                // set our frame pointer
         adr     x19, message          // address of message
 loop:
@@ -25,10 +28,11 @@ loop:
         mov     w2, 1                 // no, one char
         mov     x1, x19               // address of char
         mov     x0, STDOUT            // write on screen
-        bl      write
+        svc     WRITE
         add     x19, x19, 1           // increment pointer
         b       loop                  // and continue
 done:
         mov     w0, wzr               // return value
+        ldr     x19, [sp, save19]     // restore reg
         ldp     fp, lr, [sp], FRAME   // undo our stack frame
         ret
