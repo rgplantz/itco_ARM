543201f011b31d8db6e4448fbd179469c80f3003
rgplantz <rgplantz@outlook.com>
2024-03-07 15:18:09 -0800

    Update blink_led.py

--- chapter_20/your_turn/blink_led_python/blink_led.py
+++ chapter_20/your_turn/blink_led_python/blink_led.py
@@ -1,13 +1,17 @@
 # Blinks LED connected to GPIO pin 17
-from gpiozero import LED
-from time import sleep
+import gpiod
+import time
 
-led = LED(17)
+LED_PIN = 17
+chip = gpiod.Chip('gpiochip4')
+led_line = chip.get_line(LED_PIN)
+led_line.request(consumer = "LED", type = gpiod.LINE_REQ_DIR_OUT)
 
 for _ in range(5):
-    print("led on...")
-    led.on()
-    sleep(3)
-    print("...led off")
-    led.off()
-    sleep(3)
+    print("led on")
+    led_line.set_value(1)
+    time.sleep(3)
+    print("led off")
+    led_line.set_value(0)
+    time.sleep(3)
+led_line.release()
