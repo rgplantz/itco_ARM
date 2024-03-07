cea2a034cd70f1e4d17505631834b9b1103fdbfb
rgplantz <rgplantz@outlook.com>
2024-03-07 15:19:29 -0800

    Update blink_led.py

--- chapter_20/your_turn/blink_led_python/blink_led.py
+++ chapter_20/your_turn/blink_led_python/blink_led.py
@@ -3,7 +3,7 @@ import gpiod
 import time
 
 LED_PIN = 17
-chip = gpiod.Chip('gpiochip4')
+chip = gpiod.Chip('gpiochip0')
 led_line = chip.get_line(LED_PIN)
 led_line.request(consumer = "LED", type = gpiod.LINE_REQ_DIR_OUT)
 
