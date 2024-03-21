8575a5559274d0d649936bd040c8227a5f97224c
rgplantz <rgplantz@outlook.com>
2024-03-21 14:57:03 -0700

    Update gpio_5_pin_function.s

--- chapter_20/text_listings/blink_5_led_asm/gpio_5_pin_function.s
+++ chapter_20/text_listings/blink_5_led_asm/gpio_5_pin_function.s
@@ -6,6 +6,8 @@
 //      Returns address of RIOBase
 
 // Constants
+        .equ    RIOBase, 0x10000  // offset to RIO
+        .equ    PADBase, 0x20000  // offset to
         .equ    PIN_FUNCTION, 5   // SYS_RIO
         .equ    PAD_AMPS, 0x10    // 4 ma
         .equ    RIO_SET, 0x2000   // set reg. offset
@@ -18,18 +20,20 @@
         .type   gpio_pin_to_output, %function
 gpio_pin_to_output:
         mov     w3, w1            // pin number
-        add	    x1, x0, 0x10000   // x1 = RIOBase
-        add	    x2, x0, 0x20000   // x2 = PADBase
-        add	    x2, x2, 4         // x2 = pad
+
         lsl	    x4, x3, 3         // 8 x pin number
         add	    x5, x0, x4        // GPIO_pin_number_CTRL
         mov	    w6, PIN_FUNCTION  // system registered i/o
         str	    w6, [x5, 4]       // GPIO_pin_number_STATUS = fn;
 
+        add	    x2, x0, 0x20000   // x2 = PADBase
+        add	    x2, x2, 4         // x2 = pad
         lsl	    x4, x3, 2         // 4 x pin number
         add	    x5, x2, x4        // pin_number pad reg. address 
         mov	    w6, PAD_AMPS      // 4 ma
         str	    w6, [x5]          // set pad amps
+
+        add	    x1, x0, 0x10000   // x1 = RIOBase
         mov	    w6, 1             // a bit
         lsl	    w6, w6, w3        // shift to pin location
         add	    x5, x1, RIO_SET   // use RIO set register
