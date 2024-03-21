8868c41f0bf43d478e5e69d29cb25a4355b1db84
rgplantz <rgplantz@outlook.com>
2024-03-21 12:33:00 -0700

    Change set/clr to toggle

--- chapter_20/text_listings/blink_5_led_asm/gpio_5_pin_toggle.s
+++ chapter_20/text_listings/blink_5_led_asm/gpio_5_pin_toggle.s
@@ -1,20 +1,20 @@
-// Sets GPIO pin. Assumes that GPIO registers
+// Toggles GPIO pin. Assumes that GPIO registers
 // have been mapped to programming memory.
 // Calling sequence:
 //       x0 <- address of RIOBase in mapped memory
 //       w1 <- pin number
 
 // Constants
-        .equ    RIO_SET, 0x2000     // set register
+        .equ    RIO_XOR, 0x1000     // xor register
 
 // Code
         .text
         .align  2
-        .global gpio_pin_set
-        .type   gpio_pin_set, %function
-gpio_pin_set:
+        .global gpio_pin_toggle
+        .type   gpio_pin_toggle, %function
+gpio_pin_toggle:
         mov     w2, 1               // a bit
         lsl     w2, w2, w1          // shift to pin location
-        add     x0, x0, RIO_SET     // address RIO set reg
-        str     w2, [x0]            // pin high
+        add     x0, x0, RIO_XOR     // address RIO xor reg
+        str     w2, [x0]            // toggle pin
         ret
