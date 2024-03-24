923ee6cc40c7ebd0c6cdcfaa6aeb42a3e888ce06
rgplantz <rgplantz@outlook.com>
2024-03-23 19:50:32 -0700

    Update gpio_line_to_output.s

--- chapter_20/text_listings/blink_led_asm/gpio_line_to_output.s
+++ chapter_20/text_listings/blink_led_asm/gpio_line_to_output.s
@@ -7,7 +7,7 @@
 
 // Constants for assembler
         .equ    FIELD_MASK, 0b111  // 3 bits
-        .equ    OUTPUT, 1             // use line for output
+        .equ    OUTPUT, 1          // use line for output
 
 // Code
         .text
@@ -17,15 +17,15 @@
 gpio_line_to_output:
 // Determine register and location of line function field
         mov     w3, 10          // ten fields per GPFSEL register
-        udiv    w4, w1, w3      // w4 = GPFSEL register number
+        udiv    w4, w1, w3      // GPFSEL register number
         msub    w5, w4, w3, w1  // line field number in register
 // Compute address of GPFSEL register and line field in register     
-        lsl     w4, w4, 2       // w4 = offset to GPSEL register
-        add     x7, x0, x4      // GPFSELn memory address
-        ldr     w4, [x7]        // w4 = GPFSELn register contents
+        lsl     w4, w4, 2       // offset to GPSEL register
+        add     x7, x0, x4      // GPFSEL memory address
+        ldr     w4, [x7]        // GPFSEL register contents
 
         add     w5, w5, w5, lsl 1   // 3 X line field number
-        mov     w6, FIELD_MASK  // gpio line field
+        mov     w6, FIELD_MASK  // GPIO line field
         lsl     w6, w6, w5      // shift to field bit position
         bic     w4, w4, w6      // clear current line field
 
