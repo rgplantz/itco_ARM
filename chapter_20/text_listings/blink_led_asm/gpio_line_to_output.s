d5e6d7cd46c3942e6af0fa791fc6c37b7d38feaf
rgplantz <rgplantz@outlook.com>
2024-03-23 20:09:12 -0700

    Update gpio_line_to_output.s

--- chapter_20/text_listings/blink_led_asm/gpio_line_to_output.s
+++ chapter_20/text_listings/blink_led_asm/gpio_line_to_output.s
@@ -18,20 +18,20 @@ gpio_line_to_output:
 // Determine register and location of line function field
         mov     w3, 10          // ten fields per GPFSEL register
         udiv    w4, w1, w3      // GPFSEL register number
-        msub    w5, w4, w3, w1  // line field number in register
+        msub    w5, w4, w3, w1  // relative FSEL number in register
 // Compute address of GPFSEL register and line field in register     
-        lsl     w4, w4, 2       // offset to GPSEL register
+        lsl     w4, w4, 2       // offset to GPFSEL register
         add     x7, x0, x4      // GPFSEL memory address
         ldr     w4, [x7]        // GPFSEL register contents
 
-        add     w5, w5, w5, lsl 1   // 3 X line field number
-        mov     w6, FIELD_MASK  // GPIO line field
-        lsl     w6, w6, w5      // shift to field bit position
-        bic     w4, w4, w6      // clear current line field
+        add     w5, w5, w5, lsl 1   // 3 X relative FSEL number
+        mov     w6, FIELD_MASK  // FSEL line field
+        lsl     w6, w6, w5      // shift to relative FSEL bit position
+        bic     w4, w4, w6      // clear current FSEL
 
         mov     w2, OUTPUT      // function = output
-        lsl     w2, w2, w5      // shift function code to line position
+        lsl     w2, w2, w5      // shift function code to FSEL position
         orr     w4, w4, w2      // insert function code
-        str     w4, [x7]        // update register
+        str     w4, [x7]        // update GPFSEL register
 
         ret
