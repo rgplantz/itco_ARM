eeb001c5adec54c724c4cbd0da090567eff663a6
rgplantz <rgplantz@outlook.com>
2024-03-28 11:43:33 -0700

    Stand-alone hello world

--- chapter_21/text_listings/hello_world/hello_world.s
+++ chapter_21/text_listings/hello_world/hello_world.s
@@ -4,6 +4,7 @@
         .equ    NUL, 0
         .equ    STDOUT, 1
         .equ    WRITE, 0x40
+        .equ    EXIT, 0x5d
 // Stack frame
         .equ    save19, 16
         .equ    FRAME, 32
@@ -14,26 +15,21 @@ message:
 // Code
         .text
         .align  2
-        .global main
-        .type   main, %function
-main:
-        stp     fp, lr, [sp, -FRAME]! // create our stack frame
-        str     x19, [sp, save19]     // save reg
-        mov     fp, sp                // set our frame pointer
-        adr     x19, message          // address of message
+        .global my_hello
+        .type   my_hello, %function
+my_hello:
+        adr     x1, message       // address of message
 loop:
-        ldrb    w0, [x19]             // load character
-        cmp     w0, NUL               // end of string?
-        b.eq    done                  // yes
-        mov     x2, 1                 // no, one char
-        mov     x1, x19               // address of char
-        mov     x0, STDOUT            // write on screen
+        ldrb    w3, [x1]          // load character
+        cmp     w3, NUL           // end of string?
+        b.eq    done              // yes
+        mov     x2, 1             // no, one char
+        mov     x0, STDOUT        // write on screen
         mov     x8, WRITE
-        svc     0                     // tell OS to do it
-        add     x19, x19, 1           // increment pointer
-        b       loop                  // and continue
+        svc     0                 // tell OS to do it
+        add     x1, x1, 1         // increment pointer
+        b       loop              // and continue
 done:
-        mov     w0, wzr               // return value
-        ldr     x19, [sp, save19]     // restore reg
-        ldp     fp, lr, [sp], FRAME   // undo our stack frame
-        ret
+        mov     w0, wzr           // return value
+        mov     x8, EXIT          // terminate this process
+        svc     0
