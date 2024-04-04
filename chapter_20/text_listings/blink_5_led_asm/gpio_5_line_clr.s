2b73f16c6e51824a6bfe4669644d61b3ea33157e
rgplantz <rgplantz@outlook.com>
2024-04-04 12:54:49 -0700

    Update gpio_5_line_clr.s

--- chapter_20/text_listings/blink_5_led_asm/gpio_5_line_clr.s
+++ chapter_20/text_listings/blink_5_led_asm/gpio_5_line_clr.s
@@ -10,9 +10,9 @@
 // Code
         .text
         .align  2
-        .global gpio_line_clr
-        .type   gpio_line_clr, %function
-gpio_line_clr:
+        .global gpio_5_line_clr
+        .type   gpio_5_line_clr, %function
+gpio_5_line_clr:
         mov     w2, 1               // a bit
         lsl     w2, w2, w1          // shift to line location
         add     x0, x0, RIO_CLR     // address RIO clear reg
