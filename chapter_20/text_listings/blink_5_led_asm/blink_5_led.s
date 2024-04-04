8d0cd790eea7eaafa802c90282bc3f9eb63f4bc7
rgplantz <rgplantz@outlook.com>
2024-04-04 11:03:17 -0700

    Update blink_5_led.s

--- chapter_20/text_listings/blink_5_led_asm/blink_5_led.s
+++ chapter_20/text_listings/blink_5_led_asm/blink_5_led.s
@@ -10,7 +10,6 @@
 // Note that the values are specified in octal.
         .equ    O_RDWR, 00000002          // open for read/write
         .equ    O_SYNC, 04010000          // ensure reads and writes are
-        .equ    O_CLOEXEC, 02000000       //    synchronized by the OS
 // The following are defined in /usr/include/asm-generic/mman-common.h.
         .equ    PROT_READ, 0x1            // page can be read
         .equ    PROT_WRITE, 0x2           // page can be written
