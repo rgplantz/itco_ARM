152b4cf19c5c63a0675729f936ae76fb7cdbaf8b
rgplantz <rgplantz@outlook.com>
2024-07-02 16:50:03 -0700

    Add C main function.

---
+++ chapter_15/text_listings/sum_ints_overflow_asm/add_two.h
@@ -0,0 +1,6 @@
+// Add two integers and return 1 for overflow, 0 for no overflow.
+
+#ifndef ADD_TWO_H
+#define ADD_TWO_H
+int add_two(int *a, int b, int c);
+#endif
---
+++ chapter_15/text_listings/sum_ints_overflow_asm/sum_ints.c
@@ -0,0 +1,19 @@
+// Add two integers and show if there is overflow.
+#include <stdio.h>
+#include "add_two.h"
+
+int main(void)
+{
+    int x, y, z, overflow;
+  
+    printf("Enter an integer: ");
+    scanf("%i", &x);
+    printf("Enter an integer: ");
+    scanf("%i", &y);
+    overflow = add_two(&z, x, y);
+    printf("%i + %i = %i\n", x, y, z);
+    if (overflow)
+        printf("** Overflow occurred **\n");
+
+    return 0;
+}
