fc851dad1ae2e2b7d4788e1060a6512779cebf8a
rgplantz <rgplantz@outlook.com>
2024-07-02 16:15:43 -0700

    Fixed some code spacing.

--- chapter_15/text_listings/factorial_C/factorial.c
+++ chapter_15/text_listings/factorial_C/factorial.c
@@ -4,10 +4,11 @@
 
 unsigned int factorial(unsigned int n)
 {
-    unsigned int current = 1; // assume base case
+    unsigned int current = 1; // Assume base case
 
-    if (n != 0)
+    if (n != 0) {
         current = n * factorial(n - 1);
+    }
 
     return current;
 }
