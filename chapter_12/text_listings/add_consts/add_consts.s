53fa60688851d799a829029a4f45fdf7f1c4d8d9
rgplantz <rgplantz@outlook.com>
2024-07-02 08:38:20 -0700

    Fix comments.

--- chapter_12/text_listings/add_consts/add_consts.s
+++ chapter_12/text_listings/add_consts/add_consts.s
@@ -4,7 +4,7 @@
         .equ    z, 28
         .equ    FRAME, 32
 // Constant data 
-        .section  .rodata
+        .section        .rodata
 format:
         .string "%i + %i + 456 = %i\n"
 // Code
@@ -13,22 +13,22 @@ format:
         .global	main
         .type	main, %function
 main:
-        stp     fp, lr, [sp, FRAME]!  // Our stack frame
+        stp     fp, lr, [sp, FRAME]!  // Create stack frame
         mov     fp, sp
 
-        mov     w19, 123              // First constant
-        mov     w20, -123             // Second constant
-        add     w21, w19, w20         // Add them.
+        mov     w19, 123              // 1st constant
+        mov     w20, -123             // 2nd constant
+        add     w21, w19, w20         // Add them
         add     w22, w21, 456         // Another constant
-        str     w22, [sp, z]          // Store sum.
+        str     w22, [sp, z]          // Store sum
 
-        ldr     w3, [sp, z]           // Get sum.
-        mov     w2, w20               // Get 2nd constant.
+        ldr     w3, [sp, z]           // Get sum
+        mov     w2, w20               // Get 2nd constant
         orr     w2, wzr, w20          // Alias
-        mov     w1, w19               // Get 1st constant.
-        adr     x0, format            // Assume on same page.
+        mov     w1, w19               // Get 1st constant
+        adr     x0, format            // Assume on same page
         bl      printf
 
-        mov     w0, wzr               // Return 0.
-        ldp     fp, lr, [sp], FRAME   // Undo stack FRAME.
+        mov     w0, wzr               // Return 0
+        ldp     fp, lr, [sp], FRAME   // Delete stack frame
         ret
