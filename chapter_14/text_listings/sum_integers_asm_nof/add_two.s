067ee3c48517c9c8d0e01cf52a3c4b4e068f7a0e
rgplantz <rgplantz@outlook.com>
2024-07-02 12:19:18 -0700

    Fix comments.

--- chapter_14/text_listings/sum_integers_asm_nof/add_two.s
+++ chapter_14/text_listings/sum_integers_asm_nof/add_two.s
@@ -1,9 +1,9 @@
-// Adds two integers and outputs sum.
+// Add two integers and output the sum.
 // Calling sequence:
 //    x0 <- address of output
 //    w1 <- integer
 //    w2 <- integer
-//    returns 0
+//    Returns 0
         .arch armv8-a
 // Stack frame
         .equ    save1920, 16
@@ -14,15 +14,15 @@
         .global add_two
         .type   add_two, %function
 add_two:
-        stp     fp, lr, [sp, -FRAME]! // create our stack frame
-        mov     fp, sp                // set our frame pointer
-        stp     x19, x20, [sp, save1920]  // save for local vars
+        stp     fp, lr, [sp, -FRAME]!     // Create stack frame
+        mov     fp, sp                    // Set our frame pointer
+        stp     x19, x20, [sp, save1920]  // Save for local vars
 
-        mov     x20, x0               // for output
-        add     w19, w2, w1           // compute sum
-        str     w19, [x20]            // output sum
+        mov     x20, x0                   // For output
+        add     w19, w2, w1               // Compute sum
+        str     w19, [x20]                // Output sum
 
-        mov     w0, wzr               // return 0
-        ldp     x19, x20, [sp, save1920]  // restore regs
-        ldp     fp, lr, [sp], FRAME   // restore fp, lr, sp
-        ret                           // back to caller
+        mov     w0, wzr                   // Return 0
+        ldp     x19, x20, [sp, save1920]  // Restore reg
+        ldp     fp, lr, [sp], FRAME       // Delete stack frame
+        ret                               // Back to caller
