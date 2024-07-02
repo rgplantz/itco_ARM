fa5a2632e1f449749e1870044b4a0430bdc1f8f2
rgplantz <rgplantz@outlook.com>
2024-07-02 10:27:51 -0700

    Fix comments.

--- chapter_13/text_listings/switch_asm/switch.s
+++ chapter_13/text_listings/switch_asm/switch.s
@@ -1,8 +1,8 @@
 // Select one of three or default.
         .arch armv8-a
 // Useful names
-        .equ    N_TIMES, 10           // number of loops
-        .equ    DEFAULT, 4            // default case
+        .equ    N_TIMES, 10           // Number of loops
+        .equ    DEFAULT, 4            // Default case
 // Stack frame
         .equ    save1920, 16
         .equ    FRAME, 32
@@ -19,8 +19,8 @@ over_msg:
 // Branch table
         .align  3
 br_table:
-        .quad   one                   // addresses where messages
-        .quad   two                   // are printed
+        .quad   one                   // Addresses where messages
+        .quad   two                   //   are printed
         .quad   three
         .quad   default
 // Program code
@@ -29,41 +29,41 @@ br_table:
         .global main
         .type   main, %function
 main:
-        stp     fp, lr, [sp, -FRAME]! // create our stack frame
-        mov     fp, sp                // set our frame pointer
-        stp     x19, x20, [sp, save1920]  // save for caller
+        stp     fp, lr, [sp, -FRAME]! // Create stack frame
+        mov     fp, sp                // Set our frame pointer
+        stp     x19, x20, [sp, save1920]  // Save for caller
         mov     x19, 1                // i = 1
-        mov     x20, DEFAULT          // default case
+        mov     x20, DEFAULT          // Default case
 loop:
-        cmp     x19, N_TIMES          // is i at end?
-        b.hi    done                  // yes, leave loop
-        adr     x0, br_table          // address of branch table
-        cmp     x19, x20              // default case?
-        csel    x1, x19, x20, lo      // low, use i
-        sub     x1, x1, 1             // relative to first table entry
-        add     x0, x0, x1, lsl 3     // add address offset in table
-        ldr     x0, [x0]              // load address from table
-        br      x0                    //     and branch there
+        cmp     x19, N_TIMES          // Is i at end?
+        b.hi    done                  // Yes, leave loop
+        adr     x0, br_table          // Address of branch table
+        cmp     x19, x20              // Default case?
+        csel    x1, x19, x20, lo      // Low, use i
+        sub     x1, x1, 1             // Relative to first table entry
+        add     x0, x0, x1, lsl 3     // Add address offset in table
+        ldr     x0, [x0]              // Load address from table
+        br      x0                    //   and branch there
 one:
         adr     x0, one_msg           // = 1
-        bl      puts                  // write on screen
+        bl      puts                  // Write to screen
         b       continue
 two:
         adr     x0, two_msg           // = 2
-        bl      puts                  // write on screen
+        bl      puts                  // Write to screen
         b       continue
 three:
         adr     x0, three_msg         // = 3
-        bl      puts                  // write on screen
+        bl      puts                  // Write to screen
         b       continue
 default:
-        adr     x0, over_msg// > 3
-        bl      puts                  // write on screen
+        adr     x0, over_msg          // > 3
+        bl      puts                  // Write to screen
 continue:
-        add     x19, x19, 1           // i++;
-        b       loop                  // and continue loop
+        add     x19, x19, 1           // increment i
+        b       loop                  //   and continue loop
 done:
-        mov     w0, wzr               // return 0
-        ldp     x19, x20, [sp, save1920]  // restore reg.
-        ldp     fp, lr, [sp], FRAME   // restore fp, lr, sp
-        ret                           // back to caller
+        mov     w0, wzr               // Return 0
+        ldp     x19, x20, [sp, save1920]  // Restore reg
+        ldp     fp, lr, [sp], FRAME   // Delete stack frame
+        ret                           // Back to caller
