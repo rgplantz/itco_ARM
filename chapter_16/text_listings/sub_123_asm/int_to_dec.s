7bcc12a7e49c96ef9689bb69c81af8983a492401
rgplantz <rgplantz@outlook.com>
2024-07-02 20:22:23 -0700

    Fix comments.

--- chapter_16/text_listings/sub_123_asm/int_to_dec.s
+++ chapter_16/text_listings/sub_123_asm/int_to_dec.s
@@ -1,55 +1,53 @@
-// Converts an int to corresponding decimal text
-// string representation.
+// Convert an int to its decimal text string representation.
 // Calling sequence
 //    x0 <- place to store string
 //    w1 <- the int
-//    returns number of characters in string
+//    Return number of characters in the string.
         .arch armv8-a
 // Useful constants
-        .equ    RADIX, 10             // number base
-        .equ    INT2CHAR, 0x30        // ascii zero
-        .equ    MINUS, '-             // minus sign
-// Stack frame
+        .equ    RADIX, 10             // Number base
+        .equ    INT2CHAR, 0x30        // ASCII zero
+        .equ    MINUS, '-             // Minus sign
 // Stack frame
         .equ    reverse, 0
-        .equ    frame, 16
+        .equ    FRAME, 16
 // Code
         .text
         .align  2
         .global int_to_dec
         .type   int_to_dec, %function
 int_to_dec:
-        sub     sp, sp, frame         // local string on stack
+        sub     sp, sp, FRAME         // Local string on stack
 
         cmp     w1, wzr               // => 0?
-        tbz     w1, 31, non_negative  // yes, go to conversion
-        neg     w1, w1                // no, negate int
+        tbz     w1, 31, non_negative  // Yes, go to conversion
+        neg     w1, w1                // No, negate int
         mov     w2, MINUS
-        strb    w2, [x0]              // start with minus sign
-        add     x0, x0, 1             // increment pointer
+        strb    w2, [x0]              // Start with minus sign
+        add     x0, x0, 1             // Increment pointer
 non_negative:
-        add     x3, sp, reverse       // pointer to local string storage
-        strb    wzr, [x3]             // create end with NUL
-        mov     w2, RADIX             // put in register
+        add     x3, sp, reverse       // Pointer to local string storage
+        strb    wzr, [x3]             // Create end with NUL
+        mov     w2, RADIX             // Put in register
 do_while:
-        add     x3, x3, 1             // increment local pointer
-        udiv    w4, w1, w2            // compute quotient
-        msub    w5, w4, w2, w1        // remainder = quotient - RADIX * quotient
-        orr     w5, w5, INT2CHAR      // convert to ascii
-        strb    w5, [x3]              // store character
-        mov     w1, w4                // remove remainder
-        cbnz    w1, do_while          // continue if more left
+        add     x3, x3, 1             // Increment local pointer
+        udiv    w4, w1, w2            // Quotient = dividend / RADIX
+        msub    w5, w4, w2, w1        // Rem. = dividend - RADIX * quot.
+        orr     w5, w5, INT2CHAR      // Convert to ASCII
+        strb    w5, [x3]              // Store character
+        mov     w1, w4                // Remove remainder
+        cbnz    w1, do_while          // Continue if more left
 
         mov     w6, wzr               // count = 0
 copy:
-        ldrb    w5, [x3]              // load character
-        strb    w5, [x0]              // store it
-        add     x0, x0, 1             // increment to pointer
-        sub     x3, x3, 1             // decrement from pointer
-        add     w6, w6, 1             // increment counter
-        cbnz    w5, copy              // continue until NUL char
-        strb    w5, [x0]              // store NUL character
+        ldrb    w5, [x3]              // Load character
+        strb    w5, [x0]              // Store it
+        add     x0, x0, 1             // Increment to pointer
+        sub     x3, x3, 1             // Decrement from pointer
+        add     w6, w6, 1             // Increment counter
+        cbnz    w5, copy              // Continue until NUL char
+        strb    w5, [x0]              // Store NUL character
 
-        mov     w0, w6                // return count;
-        add     sp, sp, frame         // restore sp
-        ret                           // back to caller
+        mov     w0, w6                // Return count
+        add     sp, sp, FRAME         // Delete stack frame
+        ret                           // Back to caller
--- chapter_16/text_listings/sub_123_asm/sub_123.s
+++ chapter_16/text_listings/sub_123_asm/sub_123.s
@@ -1,17 +1,17 @@
-// Subtracts 123 from an integer
+// Subtract 123 from an integer.
         .arch armv8-a
 // Useful constants
-        .equ    CONSTANT, 123             // number to subtract
-        .equ    MAX, 11                   // maximum digits
+        .equ    CONSTANT, 123         // Number to subtract
+        .equ    MAX, 11               // Maximum digits
 // Stack frame
         .equ    the_int, 16
         .equ    the_string, 20
-        .equ    frame, 32
+        .equ    FRAME, 32
 // Code
         .text
         .section  .rodata
         .align  3
-prompt:
+        prompt:
         .string "Enter an integer: "
 message:
         .string "The result is: "
@@ -20,31 +20,31 @@ message:
         .global main
         .type   main, %function
 main:
-        stp     fp, lr, [sp, -frame]! // create our stack frame
-        mov     fp, sp                // set our frame pointer
+        stp     fp, lr, [sp, -FRAME]! // Create stack frame
+        mov     fp, sp                // Our frame pointer
 
-        adr     x0, prompt            // ask user for input
-        bl      write_str
+        adr     x0, prompt            // Prompt message
+        bl      write_str             // Ask for input
         add     x0, sp, the_string
         mov     w1, MAX
         bl      read_str
 
-        add     x1, sp, the_string    // input
-        add     x0, sp, the_int       // place for output
-        bl      dec_to_int            // convert as int
+        add     x1, sp, the_string    // Input
+        add     x0, sp, the_int       // Place for output
+        bl      dec_to_int            // Convert to int
 
         ldr     w1, [sp, the_int]
-        sub     w1, w1, CONSTANT      // subtract our constant
-        add     x0, sp, the_string    // place for output
-        bl      int_to_dec            // convert to text string
+        sub     w1, w1, CONSTANT      // Subtract our constant
+        add     x0, sp, the_string    // Place for output
+        bl      int_to_dec            // Convert to text string
 
-        adr     x0, message           // tell user that
+        adr     x0, message           // Tell user that
         bl      write_str
-        add     x0, sp, the_string    //    this is the result
+        add     x0, sp, the_string    //   this is the result
         bl      write_str
         mov     w0, '\n'
         bl      write_char
 
-        mov     w0, wzr               // return 0;
-        ldp     fp, lr, [sp], frame   // restore fp, lr, sp
-        ret                           // back to caller
+        mov     w0, wzr               // Return 0
+        ldp     fp, lr, [sp], FRAME   // Delete stack frame
+        ret                           // Back to caller
