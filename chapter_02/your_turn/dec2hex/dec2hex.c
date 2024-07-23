e2141c7304883d9a8843df9c4058b9e4f807926a
rgplantz <rgplantz@outlook.com>
2024-07-23 16:49:36 -0700

    Create dec2hex.c

---
+++ chapter_02/your_turn/dec2hex/dec2hex.c
@@ -0,0 +1,15 @@
+// Convert from decimal to hexadecimal.
+
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned int value;
+
+    printf("Decimal: ");
+    scanf("%u", &value);
+    printf("%u = 0x%02x\n", value, value);
+
+    return 0;
+}
+
