1b2e7dffd40c23ca320083977b6b5300e7b46689
rgplantz <rgplantz@outlook.com>
2024-08-08 14:34:05 -0700

    Update test_chars.c

--- chapter_10/your_turn/return_chars/test_chars.c
+++ chapter_10/your_turn/return_chars/test_chars.c
@@ -1,22 +1,19 @@
-/* test_chars.c
- * Tests three functions that return chars.
- */
+// Tests three functions that return chars.
 
 #include <stdio.h>
 #include "exclaim.h"
-#include "upperOh.h"
+#include "upper_oh.h"
 #include "tilde.h"
 
 int main(void)
 {
-  char return1, return2, return3;
+    char return1, return2, return3;
 
-  return1 = exclaim();
-  return2 = upperOh();
-  return3 = tilde();
-  printf("The returned chars are: %c, %c, and %c.\n",
-         return1, return2, return3);
+    return1 = exclaim();
+    return2 = upper_oh();
+    return3 = tilde();
+    printf("The returned chars are: %c, %c, and %c.\n",
+          return1, return2, return3);
 
-  return 0;
+    return 0;
 }
-
