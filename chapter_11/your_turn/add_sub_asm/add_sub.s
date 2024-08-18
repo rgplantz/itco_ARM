c3e7d647a1dd41ba228b51896683d7048e4fedd8
rgplantz <rgplantz@outlook.com>
2024-08-18 15:51:08 -0700

    Add Your Turn 11.2

---
+++ chapter_11/your_turn/add_sub_asm/add_sub.s
@@ -0,0 +1,55 @@
+// Add and subtract two integers.
+        .arch armv8-a
+// Stack frame
+        .equ    x, 16
+        .equ    y, 20
+        .equ    sum, 24
+        .equ    diff, 28
+        .equ    FRAME, 32
+// Constants 
+        .section .rodata
+prompt:
+        .string "Enter an integer: "
+input_format:
+        .string "%i"
+result:
+        .string "Sum = %i, Difference = %i\n"
+// Code
+        .text
+        .align  2
+        .global main
+        .type   main, %function
+main:
+        stp     fp, lr, [sp, FRAME]!  // Create stack frame
+        mov     fp, sp                // Set our frame pointer
+
+        adr     x0, prompt            // prompt user
+        bl      printf
+        add     x1, sp, x             // address for input
+        adr     x0, input_format      // scanf format string
+        bl      scanf
+
+        adr     x0, prompt            // prompt user
+        bl      printf
+        add     x1, sp, y             // address for input
+        adr     x0, input_format      // scanf format string
+        bl      scanf
+
+        ldr     w0, [sp, x]           // get x
+        ldr     w1, [sp, y]           // and y
+        add     w3, w0, w1            // add them
+        str     w3, [sp, sum]         // sum = x + y
+
+        ldr     w0, [sp, x]           // get x
+        ldr     w1, [sp, y]           // and y
+        sub     w3, w0, w1            // subtract them
+        str     w3, [sp, diff]        // diff = x - y
+
+        ldr     w2, [sp, diff]        // sum
+        ldr     w1, [sp, sum]         // difference
+        adr     x0, result            // address of format string
+        bl      printf
+
+        mov     w0, wzr
+        ldp     fp, lr, [sp], FRAME   // Delete stack frame
+        ret
