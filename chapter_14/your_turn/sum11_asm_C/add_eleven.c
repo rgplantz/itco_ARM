6ae593006ac57f331c1029ed4ab8acc28c7bcd9e
rgplantz <rgplantz@outlook.com>
2024-09-11 11:58:04 -0700

    Add YT 14.3

---
+++ chapter_14/your_turn/sum11_asm_C/add_eleven.c
@@ -0,0 +1,14 @@
+// Add 11 integers and return the sum.
+
+#include "add_eleven.h"
+
+int add_eleven(int one, int two, int three, int four, int five, int six,
+                int seven, int eight, int nine, int ten, int eleven)
+{
+    int sum;
+
+    sum = one + two + three + four + five + six
+            + seven + eight + nine + ten + eleven;
+
+    return sum;
+}
---
+++ chapter_14/your_turn/sum11_asm_C/add_eleven.h
@@ -0,0 +1,7 @@
+// Add 11 integers and return the sum.
+
+#ifndef ADD_ELEVEN_H
+#define ADD_ELEVEN_H
+int add_eleven(int one, int two, int three, int four, int five, int six,
+                int seven, int eight, int nine, int ten, int eleven);
+#endif
