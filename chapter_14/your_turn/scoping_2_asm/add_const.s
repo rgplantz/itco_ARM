67bc3b0045c0ab897097a978df0766baf91cbbae
rgplantz <rgplantz@outlook.com>
2024-09-11 15:42:39 -0700

    Update add_const.s

--- chapter_14/your_turn/scoping_2_asm/add_const.s
+++ chapter_14/your_turn/scoping_2_asm/add_const.s
@@ -14,39 +14,48 @@
         .size   y, 4
 y:
         .word   INIT_Y
+        .align  2
+        .type   count, %object
+        .size   count, 4
+count:
+        .word   0
         .section  .rodata
 msg:
-        .string "In add_const:%7i %8i %8i\n"
+        .string "In add_const:%7i %8i %8i, call number %i\n"
         .text
         .align  2
         .global add_const
         .type   add_const, %function
 add_const:
-        stp     fp, lr, [sp, -FRAME]!   // create our stack frame
-        mov     fp, sp                  // set our frame pointer
+        stp     fp, lr, [sp, -FRAME]!   // Create stack frame
+        mov     fp, sp                  // Set frame pointer
 
         mov     w0, INIT_X
-        add     w0, w0, ADDITION        // add constant
-        str     w0, [sp, x]             // x += ADDITION;
+        add     w0, w0, ADDITION        // Add constant
+        str     w0, [sp, x]             // x += ADDITION
         adr     x0, y
-        ldr     w1, [x0]                // load our y
-        add     w1, w1, ADDITION        // add constant
-        str     w1, [x0]                // y += ADDITION;    
+        ldr     w1, [x0]                // Load our y
+        add     w1, w1, ADDITION        // Add constant
+        str     w1, [x0]                // y += ADDITION    
         adrp    x0, :got:z              // z page number
         ldr     x0, [x0, :got_lo12:z]   // z address
         ldr     w1, [x0]                // load z
         add     w1, w1, ADDITION        // add constant
         str     w1, [x0]                // z += ADDITION;
 
+        adr     x0, count
+        ldr     w4, [x0]                // Load our count
+        add     w4, w4, 1               // Increment
+        str     w4, [x0]    
         adrp    x0, :got:z              // z page number
         ldr     x0, [x0, :got_lo12:z]   // z address
         ldr     w3, [x0]                // load z
         adr     x0, y
-        ldr     w2, [x0]                // load our y
+        ldr     w2, [x0]                // Load our y
         ldr     w1, [sp, x]             // load our x
         adr     x0, msg                 // message
         bl      printf
 
-        mov     w0, wzr                 // return 0
-        ldp     fp, lr, [sp], FRAME     // restore fp, lr, sp
-        ret                             // back to caller
+        mov     w0, wzr                `// Return 0
+        ldp     fp, lr, [sp], FRAME     // Delete stack frame
+        ret
