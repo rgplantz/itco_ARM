6dd2e735d2ce40808b5780f1ad03f31288a38173
rgplantz <rgplantz@outlook.com>
2024-09-11 14:36:14 -0700

    Add YT 14.3

---
+++ chapter_14/your_turn/sum_ints_asm/add_ints.s
@@ -0,0 +1,27 @@
+// Add all integers between two integers and return the sum.
+// Calling sequence:
+//    w0 <- one integer
+//    w1 <- another integer
+//    Returns sum
+        .arch armv8-a
+        .text
+        .align  2
+        .global add_ints
+        .type   add_ints, %function
+add_ints:
+        cmp     w0, w1                // Check for lower
+        b.lo    in_order
+        mov     w2, w0                // Out of order, swap
+        mov     w0, w1
+        mov     w1, w2
+in_order:
+        mov     w2, w0                // Accumulate sum in w2
+loop:
+        cmp     w0, w1                // Added all of them?
+        b.hi    done                  // Yes
+        add     w0, w0, 1             // Next int
+        add     w2, w0                // Add to accmulator
+        b       loop                  // And keep adding
+
+        mov     w0, w2                // Return sum
+        ret                           // Back to caller
---
+++ chapter_14/your_turn/sum_ints_asm/sum_ints.s
@@ -0,0 +1,47 @@
+// Sum the integers between two entered by user.
+        .arch armv8-a
+// Stack frame
+        .equ    x, 16
+        .equ    y, 20
+        .equ    FRAME, 32
+// Constant data
+        .section  .rodata
+        .align  3
+prompt:
+        .string "Enter an integer: "
+in_format:
+        .string "%i"
+out_format:
+        .string "The sum is %i\n"
+        .text
+        .align  2
+        .global main
+        .type   main, %function
+main:
+        sub     sp, sp, FRAME               // Allocate our stack frame
+        stp     fp, lr, [sp, frame_record]  // Create frame record
+        mov     fp, sp                      // Set our frame pointer
+
+        adr     x0, prompt                  // Ask for integer 
+        bl      printf
+        add     x1, sp, x                   // Place for first int
+        adr     x0, in_format               // scanf format string
+        bl      scanf                       // Get the int
+
+        adr     x0, prompt                  // Ask for integer 
+        bl      printf
+        add     x1, sp, y                   // Place for second int
+        adr     x0, in_format               // scanf format string
+        bl      scanf                       // Get the int
+
+        ldr     w0, x[sp]
+        ldr     w1, y[sp]
+        bl      add_ints                    // Add them
+        mov     w1, w0
+        adr     x0, out_format
+        bl      printf
+
+        mov     w0, wzr                     // Return 0
+        ldp     fp, lr, [sp, frame_record]  // Restore fp and lr
+        add     sp, sp, FRAME               // Delete stack frame
+        ret                                 // Back to caller
