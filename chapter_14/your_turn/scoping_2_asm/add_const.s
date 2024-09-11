5c9ba974f3beca4aeeacfc5be202b8139c63ef0e
rgplantz <rgplantz@outlook.com>
2024-09-11 15:17:37 -0700

    Add YT 14.5

---
+++ chapter_14/your_turn/scoping_2_asm/add_const.s
@@ -0,0 +1,52 @@
+// Add constant to automatic, static, global variables.
+        .arch armv8-a
+// Useful names
+        .equ    INIT_X, 78
+        .equ    INIT_Y, 90
+        .equ    ADDITION, 1000
+// Stack frame
+        .equ    x, 28
+        .equ    FRAME, 32
+// Code
+        .data
+        .align  2
+        .type   y, %object
+        .size   y, 4
+y:
+        .word   INIT_Y
+        .section  .rodata
+msg:
+        .string "In add_const:%7i %8i %8i\n"
+        .text
+        .align  2
+        .global add_const
+        .type   add_const, %function
+add_const:
+        stp     fp, lr, [sp, -FRAME]!   // create our stack frame
+        mov     fp, sp                  // set our frame pointer
+
+        mov     w0, INIT_X
+        add     w0, w0, ADDITION        // add constant
+        str     w0, [sp, x]             // x += ADDITION;
+        adr     x0, y
+        ldr     w1, [x0]                // load our y
+        add     w1, w1, ADDITION        // add constant
+        str     w1, [x0]                // y += ADDITION;    
+        adrp    x0, :got:z              // z page number
+        ldr     x0, [x0, :got_lo12:z]   // z address
+        ldr     w1, [x0]                // load z
+        add     w1, w1, ADDITION        // add constant
+        str     w1, [x0]                // z += ADDITION;
+
+        adrp    x0, :got:z              // z page number
+        ldr     x0, [x0, :got_lo12:z]   // z address
+        ldr     w3, [x0]                // load z
+        adr     x0, y
+        ldr     w2, [x0]                // load our y
+        ldr     w1, [sp, x]             // load our x
+        adr     x0, msg                 // message
+        bl      printf
+
+        mov     w0, wzr                 // return 0
+        ldp     fp, lr, [sp], FRAME     // restore fp, lr, sp
+        ret                             // back to caller
---
+++ chapter_14/your_turn/scoping_2_asm/var_life.s
@@ -0,0 +1,63 @@
+// Compare scope and lifetime of automatic, static,
+// and global variables.
+        .arch armv8-a
+// Useful names
+        .equ    INIT_X, 12
+        .equ    INIT_Y, 34
+        .equ    INIT_Z, 56
+// Stack frame
+        .equ    x, 24
+        .equ    y, 28
+        .equ    FRAME, 32
+// Code
+        .global z
+        .data
+        .align  2
+        .type   z, %object
+        .size   z, 4
+z:
+        .word   INIT_Z
+        .section  .rodata
+heading0:
+        .string "           automatic   static   global"
+heading1:
+        .string "                   x        y        z"
+msg:
+        .string "In main:%12i %8i %8i\n"
+        .text
+        .align  2
+        .global main
+        .type   main, %function
+main:
+        stp     fp, lr, [sp, -FRAME]!   // create our stack frame
+        mov     fp, sp                  // set our frame pointer
+
+        mov     w0, INIT_X
+        str     w0, [sp, x]             // x = INIT_X;
+        mov     w0, INIT_Y
+        str     w0, [sp, y]             // y = INIT_Y;
+        adr     x0, heading0            // print 2-line header
+        bl      puts
+        adr     x0, heading1
+        bl      puts
+
+        adr     x0, z 
+        ldr     w3, [x0]                // global z
+        ldr     w2, [sp, y]             // local y
+        ldr     w1, [sp, x]             // and local x
+        adr     x0, msg                 // show values
+        bl      printf
+
+        bl      add_const               // add constants
+        bl      add_const               //     twice
+
+        adr     x0, z                   // repeat display
+        ldr     w3, [x0]
+        ldr     w2, [sp, y]
+        ldr     w1, [sp, x]
+        adr     x0, msg
+        bl      printf
+
+        mov     w0, wzr                 // return 0
+        ldp     fp, lr, [sp], FRAME     // restore fp, lr, sp
+        ret                             // back to caller
