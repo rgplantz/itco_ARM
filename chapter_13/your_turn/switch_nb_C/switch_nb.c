fb70f5041d55e4637f1f9805645539d780bd9517
rgplantz <rgplantz@outlook.com>
2024-09-11 16:03:06 -0700

    Add YT 13.4

--- chapter_13/your_turn/switch_nb_C/switch_nb.c
+++ chapter_13/your_turn/switch_nb_C/switch_nb.c
@@ -1,29 +1,25 @@
-/* switch_nb.c
- * Three-way selection.
- */
+// Three-way selection.
 
 #include <stdio.h>
 
 int main(void)
 {
-  register int selector;
-  register int i;
+    register int selector;
+    register int i;
 
-  for (i = 1; i <= 5; i++)
-  {
-    selector = i;
-    switch (selector)
-    {
-      case 1:
-        puts("i = 1");
-      case 2:
-        puts("i = 2");
-      case 3:
-        puts("i = 3");
-      default:
-        puts("i > 3");
+    for (i = 1; i <= 5; i++) {
+      selector = i;
+      switch (selector) {
+        case 1:
+          puts("i = 1");
+        case 2:
+          puts("i = 2");
+        case 3:
+          puts("i = 3");
+        default:
+          puts("i > 3");
+      }
     }
-  }
 
-  return 0;
+    return 0;
 }
