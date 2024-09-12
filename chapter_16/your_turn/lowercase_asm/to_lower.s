d299b6f593f652e1df8a3853ad4c0dbda6e03a34
rgplantz <rgplantz@outlook.com>
2024-09-12 11:52:28 -0700

    Update to_lower.s

--- chapter_16/your_turn/lowercase_asm/to_lower.s
+++ chapter_16/your_turn/lowercase_asm/to_lower.s
@@ -9,13 +9,13 @@
 // Program code
         .text
         .align  2
-        .global to_upper
-        .type   to_upper, %function
-to_upper:
+        .global to_lower
+        .type   to_lower, %function
+to_lower:
         mov     w2, wzr               // counter = 0
-convertLoop:
+loop:
         ldrb    w3, [x1]              // Load character
-        cbz     w3, allDone           // All done if NUL char
+        cbz     w3, done              // All done if NUL char
         movz    w4, LOWMASK           // If not, do masking
         orr     w3, w3, w4            // Mask to lower
         strb    w3, [x0]              // Store result
@@ -23,7 +23,7 @@ convertLoop:
         add     x1, x1, 1             //   source pointer,
         add     w2, w2, 1             //   and counter,
         b       convertLoop           //   and continue
-allDone:
+done:
         strb    w3, [x0]              // Terminating NUL got us here
         mov     w0, w2                // Return count
         ret                           // Back to caller
