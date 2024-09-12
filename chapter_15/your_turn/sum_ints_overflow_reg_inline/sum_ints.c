4dc16662502dd70bb17e9832c5b915cb75c220ba
rgplantz <rgplantz@outlook.com>
2024-09-12 11:01:58 -0700

    Add YT 15.4

---
+++ chapter_15/your_turn/sum_ints_overflow_reg_inline/sum_ints.c
@@ -0,0 +1,25 @@
+// Add two integers and show if there is overflow.
+
+#include <stdio.h>
+
+int main(void)
+{
+    int x, y;
+    register int z, overflow;
+  
+    printf("Enter an integer: ");
+    scanf("%i", &x);
+    printf("Enter an integer: ");
+    scanf("%i", &y);
+
+    asm ("adds %w0, %w2, %w3\n"
+        "cinc %w1, wzr, vs"
+        : "=r" (z), "=r" (overflow)
+        : "r" (x), "r" (y));
+
+    printf("%i + %i = %i\n", x, y, z);
+    if (overflow)
+        printf("** Overflow occurred **\n");
+
+    return 0;
+}
