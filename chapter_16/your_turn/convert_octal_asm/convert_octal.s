adb6eedb987578266c7db52eee0732354d911f9e
rgplantz <rgplantz@outlook.com>
2024-09-12 15:07:05 -0700

    Update YT 16.5

---
+++ chapter_16/your_turn/convert_octal_asm/convert_octal.s
@@ -0,0 +1,43 @@
+// Get an ocatal number from the user and store it as a long int.
+        .arch armv8-a
+// Useful constant
+        .equ    MAX, 21
+// Stack frame
+        .equ    the_int, 16
+        .equ    the_string, 24
+        .equ    FRAME, 48
+// Code
+        .text
+        .section  .rodata
+        .align  3
+prompt:
+        .string "Enter up to 21 octal characters: "
+format:
+        .string "0o%x = %i\n"
+        .text
+        .align  2
+        .global main
+        .type   main, %function
+main:
+        stp     fp, lr, [sp, -FRAME]! // Create stack frame
+        mov     fp, sp                // Our frame pointer
+
+        adr     x0, prompt            // Prompt message
+        bl      write_str             // Ask for input
+
+        add     x0, sp, the_string    // Place to store string
+        mov     w1, MAX               // Limit number of input chars
+        bl      read_str              // Get from keyboard
+
+        add     x1, sp, the_string    // Address of string
+        add     x0, sp, the_int       // Place to store int
+        bl      octal_to_int          // Do conversion
+
+        ldr     w2, [sp, the_int]     // Load int
+        ldr     w1, [sp, the_int]     // printf shows this copy in hex
+        adr     x0, format            // Format string
+        bl      printf
+
+        mov     w0, 0                 // Return 0
+        ldp     x29, x30, [sp], FRAME // Delete stack frame
+        ret
---
+++ chapter_16/your_turn/convert_octal_asm/octal_to_int.s
@@ -0,0 +1,29 @@
+// Convert an octal character string to a long int.
+// Calling sequence
+//    x0 <- pointer to long int result
+//    x1 <- pointer to octal character string to convert
+//    Return number of characters converted.
+        .arch armv8-a
+// Useful constants
+        .equ    INTPART, 0x0f
+// Program code
+        .text
+        .align  2
+        .global hex_to_int
+        .type   hex_to_int, %function
+hex_to_int:
+        mov     x2, xzr               // result = 0;
+        mov     w3, wzr               // counter = 0;
+convert:
+        ldrb    x4, [x1]              // Load character
+        cbz     x4, done              // NUL character?
+        and     x4, x4, INTPART       // 4-bit integer
+        lsl     x2, x2, 3             // Make room for it
+        orr     x2, x2, x4            // Insert new 3-bit integer
+        add     x1, x1, 1             // Increment source pointer
+        add     w3, w3, 1             //   and counter
+        b       convert               //   and continue
+done:
+        str     x2, [x0]              // Output result
+        mov     w0, w3                // Return count
+        ret                           // Back to caller
