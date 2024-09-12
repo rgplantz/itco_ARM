4055eeed1718fa9c616898a428db91f56db4a536
rgplantz <rgplantz@outlook.com>
2024-09-12 10:42:27 -0700

    Update add_two.c

--- chapter_15/your_turn/sum_ints_carry_C/add_two.c
+++ chapter_15/your_turn/sum_ints_carry_C/add_two.c
@@ -8,10 +8,9 @@ unsigned int add_two(unsigned int *a, unsigned int b, unsigned int c)
     unsigned int carry = 0;   // Assume no overflow
   
     sum = b + c;
-//    if (((b > 0) && (c > 0) && (sum < 0)) ||
-//            ((b < 0) && (c < 0) && (sum > 0))) {
-//        carry = 1;
-//    }
+    if ((sum < b) || (sum < c)) {
+        carry = 1;
+    }
     *a = sum;
 
     return carry;
