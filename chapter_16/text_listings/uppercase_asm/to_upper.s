2c431713cd393dfbe350a76b8a4fc839247b8cfd
rgplantz <rgplantz@outlook.com>
2024-09-12 11:43:26 -0700

    Update to_upper.s

--- chapter_16/text_listings/uppercase_asm/to_upper.s
+++ chapter_16/text_listings/uppercase_asm/to_upper.s
@@ -13,17 +13,17 @@
         .type   to_upper, %function
 to_upper:
         mov     w2, wzr               // counter = 0
-convertLoop:
+loop:
         ldrb    w3, [x1]              // Load character
-        cbz     w3, allDone           // All done if NUL char
+        cbz     w3, done              // All done if NUL char
         movz    w4, UPMASK            // If not, do masking
         and     w3, w3, w4            // Mask to upper
         strb    w3, [x0]              // Store result
         add     x0, x0, 1             // Increment destination pointer,
         add     x1, x1, 1             //   source pointer,
         add     w2, w2, 1             //   and counter,
-        b       convertLoop           //   and continue
-allDone:
+        b       loop                  //   and continue
+done:
         strb    w3, [x0]              // Terminating NUL got us here
         mov     w0, w2                // Return count
         ret                           // Back to caller
