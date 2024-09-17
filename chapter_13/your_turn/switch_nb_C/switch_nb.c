6333d363451db218ccedf06e8e436d772c75ebb6
rgplantz <rgplantz@outlook.com>
2024-09-16 22:41:11 -0700

    Update switch_nb.c

--- chapter_13/your_turn/switch_nb_C/switch_nb.c
+++ chapter_13/your_turn/switch_nb_C/switch_nb.c
@@ -1,24 +1,23 @@
-// Three-way selection.
+// Select one of three or default.
 
 #include <stdio.h>
+#define N_TIMES 10
 
 int main(void)
 {
-    register int selector;
-    register int i;
+    int i;
 
-    for (i = 1; i <= 5; i++) {
-      selector = i;
-      switch (selector) {
+    for (i = 1; i <= N_TIMES; i++) {
+        switch (i) {
         case 1:
-          puts("i = 1");
+            puts("i = 1");
         case 2:
-          puts("i = 2");
+            puts("i = 2");
         case 3:
-          puts("i = 3");
+            puts("i = 3");
         default:
-          puts("i > 3");
-      }
+            puts("i > 3");
+        }
     }
 
     return 0;
