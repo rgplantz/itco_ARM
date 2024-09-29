91338bae7861c7221976931c9fb977cc7605af38
rgplantz <rgplantz@outlook.com>
2024-09-29 12:04:09 -0700

    Update fill_array.c

--- chapter_17/your_turn/array_pass_ptr_C/fill_array.c
+++ chapter_17/your_turn/array_pass_ptr_C/fill_array.c
@@ -1,15 +1,15 @@
-// Allocates an int array, stores 2 x element number
-// in each element and prints array contents.
+// Allocate an int array, store 2 x element number
+// in each element and print array contents.
 
-#include "twiceIndex.h"
-#include "displayArray.h"
+#include "twice_index.h"
+#include "display_array.h"
 #define N 10
 
 int main(void) {
-    int intArray[N];
+    int int_array[N];
     
-    twiceIndex(intArray, N);
-    displayArray(intArray, N);
+    twice_index(int_array, N);
+    display_array(int_array, N);
     
     return 0;
 }
