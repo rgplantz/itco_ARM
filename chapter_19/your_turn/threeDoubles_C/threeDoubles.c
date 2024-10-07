bdce3fc5471c953cf8667a8f257732b00401d065
rgplantz <rgplantz@outlook.com>
2024-10-06 17:49:37 -0700

    Update threeDoubles.c

--- chapter_19/your_turn/threeDoubles_C/threeDoubles.c
+++ chapter_19/your_turn/threeDoubles_C/threeDoubles.c
@@ -1,31 +1,28 @@
-/* threeDoubles.c
- * Associativity of doubles.
- */
+// Test the addition associativity of doubles.
 
 #include <stdio.h>
 #include <math.h>
 
 int main()
 {
-  double x, y, z, sum1, sum2;
+    double x, y, z, sum1, sum2;
 
-  printf("Enter a number: ");
-  scanf("%lf", &x);
-  printf("Enter a number: ");
-  scanf("%lf", &y);
-  printf("Enter a number: ");
-  scanf("%lf", &z);
-  
-  sum1 = x + y;
-  sum1 += z;      /* sum1 = (x + y) + z */
-  sum2 = y + z;
-  sum2 += x;      /* sum2 = x + (y + z) */
+    printf("Enter a number: ");
+    scanf("%lf", &x);
+    printf("Enter a number: ");
+    scanf("%lf", &y);
+    printf("Enter a number: ");
+    scanf("%lf", &z);
+    
+    sum1 = x + y;
+    sum1 += z;      /* sum1 = (x + y) + z */
+    sum2 = y + z;
+    sum2 += x;      /* sum2 = x + (y + z) */
 
-  if (sum1 == sum2)
-    printf("%lf is the same as %lf\n", sum1, sum2);
-  else
-    printf("%lf is not the same as %lf\n", sum1, sum2);
+    if (sum1 == sum2)
+      printf("%lf is the same as %lf\n", sum1, sum2);
+    else
+      printf("%lf is not the same as %lf\n", sum1, sum2);
 
-  return 0;
+    return 0;
 }
-
